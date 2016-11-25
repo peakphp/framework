3e2f9830e8eb370dcda5e8f12954adef86301f6b
Francois Lajoie <snake386@hotmail.com>
2016-11-25 11:15:17 -0500

    added view helper class to inject view object into view helper

---
+++ src/View/Helper.php
@@ -0,0 +1,21 @@
+<?php
+namespace Peak\View;
+
+use Peak\Registry;
+
+/**
+ * Peak View Helper Base
+ */
+abstract class Helper
+{
+
+    public $view;
+    
+    /**
+     * Get view object
+     */
+    public function __construct()
+    {
+        $this->view = Registry::o()->view;
+    } 
+}
