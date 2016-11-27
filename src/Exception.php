b46f5da6fb6bbb54ab348338602e406a4cfb89a9
Francois Lajoie <snake386@hotmail.com>
2016-11-27 11:38:08 -0500

    removed exception and moved constants to Peak\Exception

--- src/Exception.php
+++ src/Exception.php
@@ -21,11 +21,25 @@ class Exception extends \Exception
 	/**
 	 * Errors messages
 	 */
-	const ERR_ROUTER_URI_NOT_FOUND          = 'Request uri not found.';
-	const ERR_CORE_INIT_CONST_MISSING       = '%1$s is not specified (const %2$s)';
-	const ERR_DEFAULT                       = 'Request failed';
-	const ERR_CUSTOM                        = '%1$s';
-	const ERR_CONFIG_FILE                   = 'Application configuration file format invalid';
+    const ERR_ROUTER_URI_NOT_FOUND          = 'Request uri not found.';
+    const ERR_CORE_INIT_CONST_MISSING       = '%1$s is not specified (const %2$s)';
+    const ERR_DEFAULT                       = 'Request failed';
+    const ERR_CUSTOM                        = '%1$s';
+    const ERR_CONFIG_FILE                   = 'Application configuration file format invalid';
+    
+    const ERR_VIEW_ENGINE_NOT_SET           = 'View rendering engine not set. Use engine() from Peak_View before trying to render application controller.';
+    const ERR_VIEW_ENGINE_NOT_FOUND         = 'View rendering engine \'%1$s\' not found.';
+    const ERR_VIEW_HELPER_NOT_FOUND         = 'View helper \'%1$s\' not found.';
+    const ERR_VIEW_SCRIPT_NOT_FOUND         = 'View script file %1$s not found.';
+    const ERR_VIEW_FILE_NOT_FOUND           = 'View file \'%1$s\' not found.';
+    const ERR_VIEW_THEME_NOT_FOUND          = 'View theme \'%1$s\' folder not found.';
+    
+    const ERR_CTRL_NOT_FOUND                = 'Application controller %1$s not found.';
+    const ERR_CTRL_ACTION_NOT_FOUND         = 'Controller action \'%1$s\' not found in %2$s.';
+    const ERR_CTRL_DEFAULT_ACTION_NOT_FOUND = 'Controller action method by default not found.';
+    const ERR_CTRL_HELPER_NOT_FOUND         = 'Controller helper \'%1$s\' not found.';
+    const ERR_CTRL_ACTION_PARAMS_MISSING    = '%1$s argument(s) missing for action \'%2$s\'';
+    
 
     /**
      * Set error key constant
