4a63cd0b9b66a5282df857c4adda5e8e0c493501
Francois Lajoie <snake386@hotmail.com>
2016-12-01 23:13:41 -0500

    method arrayMergeRecusive() changed to static function

--- src/Config.php
+++ src/Config.php
@@ -138,16 +138,16 @@ class Config implements IteratorAggregate, Countable
      * @param  mixed $b Right array to merge over the left array
      * @return mixed
      */
-    public function arrayMergeRecursive($a, $b)
+    static function arrayMergeRecursive($a, $b)
     {
         // merge arrays if both variables are arrays
         if (is_array($a) && is_array($b)) {
             // loop through each right array's entry and merge it into $a
             foreach ($b as $key => $value) {
                 if (isset($a[$key])) {
-                    $a[$key] = $this->arrayMergeRecursive($a[$key], $value);
+                    $a[$key] = self::arrayMergeRecursive($a[$key], $value);
                 } else {
-                    if($key === 0) $a= array(0 => $this->arrayMergeRecursive($a, $value));
+                    if($key === 0) $a= array(0 => self::arrayMergeRecursive($a, $value));
                     else $a[$key] = $value;
                 }
             }
