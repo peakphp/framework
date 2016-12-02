db5ab21a96eb24b2a47fd4598a7ba423bde9994b
Francois Lajoie <snake386@hotmail.com>
2016-12-02 17:54:00 -0500

    splited bootstrap code

--- src/Application/Bootstrap.php
+++ src/Application/Bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 namespace Peak\Application;
 
+use Peak\Application;
 use Peak\Registry;
 
 /**
- * Application Bootstrapper base
+ * Application Bootstrapper
  *   
  * @author   Francois Lajoie
- * @version  $Id$
  */
 class Bootstrap
 {
@@ -17,8 +17,8 @@ class Bootstrap
      */
     public function __construct()
     {
-        $this->_configView();
-        $this->_configRouter();
+        new Bootstrap\View();
+        new Bootstrap\Router();
         $this->_boot();
     }
 
@@ -34,50 +34,6 @@ class Bootstrap
         if(!empty($c_methods)) {
             foreach($c_methods as $m) {            
                 if(substr($m, 0, $l) === $prefix) $this->$m();
-            }
-        }
-    }
-
-    /**
-     * Configure view from app config
-     */
-    protected function _configView()
-    {
-        if(!Registry::o()->app->config()->have('view') || 
-            !Registry::isRegistered('view')) return;
-
-        $view  = Registry::o()->view;
-        $cview = Registry::o()->app->config('view');
-
-        if(!empty($cview)) {
-            foreach($cview as $k => $v) {
-
-                if(is_array($v)) {
-                    foreach($v as $p1 => $p2) $view->$k($p1,$p2);
-                }
-                else $view->$k($v);
-            }
-        }
-    }
-
-    /**
-     * Configure custom routes from app config
-     */
-    protected function _configRouter()
-    {
-        if(!Registry::o()->app->config()->have('router.addregex') || 
-            !Registry::isRegistered('router')) return;
-
-        $r      = Registry::o()->router;
-        $routes = Registry::o()->app->config('router.addregex');
-
-        if(!empty($routes)) {
-            foreach($routes as $i => $exp) {
-                $parts = explode(' | ', $exp);
-                if(count($parts) == 2) {
-                    $r->addRegex(trim($parts[0]), trim($parts[1]));
-
-                }
             }
         }
     }
---
+++ src/Application/Bootstrap/View.php
@@ -0,0 +1,36 @@
+<?php
+namespace Peak\Application\Bootstrap;
+
+use Peak\Application;
+use Peak\Registry;
+
+/**
+ * Application Bootstrap View
+ */
+class View
+{
+
+    /**
+     * Configurate View based on Application config
+     */
+    public function __construct()
+    {
+        if(!Application::conf()->have('view') || 
+            !Registry::isRegistered('view')) return;
+
+
+        $view  = Registry::o()->view;
+        $cview = Application::conf('view');
+
+        if(!empty($cview)) {
+            foreach($cview as $k => $v) {
+
+                if(is_array($v)) {
+                    foreach($v as $p1 => $p2) $view->$k($p1,$p2);
+                }
+                else $view->$k($v);
+            }
+        }   
+    }
+
+}
