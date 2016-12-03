5627b550b5e67284a8c6866fb46333fa6d9c8fbd
Francois Lajoie <snake386@hotmail.com>
2016-12-03 08:39:11 -0500

    added request server uri

---
+++ src/Routing/RequestServerURI.php
@@ -0,0 +1,19 @@
+<?php
+namespace Peak\Routing;
+
+use Peak\Routing\Request;
+
+class RequestServerURI extends Request
+{
+
+    /**
+     * Overload Request consctructor using $_SERVER request uri
+     */
+    public function __construct($base_uri = null)
+    {
+        parent::__construct(
+            $_SERVER['REQUEST_URI'],
+            $base_uri
+        );    
+    }
+}
