762e98b4ad2e35d12bf3134433a9a30085a6682f
Francois Lajoie <snake386@hotmail.com>
2016-12-05 09:13:05 -0500

    updated test

--- tests/RoutingTest/RequestTest.php
+++ tests/RoutingTest/RequestTest.php
@@ -65,4 +65,19 @@ class RequestTest extends TestCase
         $request->setRequestUri('blog/category');
         $this->assertTrue($request->request_uri === $s.'blog/category'.$s);
     }
+
+    function testSeparator()
+    {
+        $s = '.'; 
+        Request::$separator = $s;
+
+        $base_raw    = 'peak....framework';
+        $request_raw = 'peak.framework.mypage';
+        $request     = new Request($request_raw, $base_raw);
+
+        $this->assertTrue($request->base_uri === $s.'peak.framework'.$s);
+        $this->assertTrue($request->request_uri === $s.'mypage'.$s);
+        $this->assertTrue($request->raw_uri === 'peak.framework.mypage');
+
+    }
 }
