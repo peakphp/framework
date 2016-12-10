6f49dbaadf0a35eebe4946d12254c46c748cb9ec
Francois Lajoie <snake386@hotmail.com>
2016-12-09 22:18:48 -0500

    added per environment functionality to application bootstrap

--- src/Application/Bootstrapper.php
+++ src/Application/Bootstrapper.php
@@ -1,19 +1,14 @@
 <?php
 namespace Peak\Application;
 
-use Peak\Application;
-use Peak\Registry;
-
 /**
  * Application Bootstrapper
- *   
- * @author   Francois Lajoie
  */
-class Bootstrap
+class Bootstrapper
 {
 
     /**
-     * init bootstrap
+     * init app bootstrap
      */
     public function __construct()
     {
@@ -28,11 +23,27 @@ class Bootstrap
      */
     private function _boot($prefix = 'init')
     {
+        $this->_env();
+
         $c_methods = get_class_methods(get_class($this));
         $l = strlen($prefix);
         if(!empty($c_methods)) {
             foreach($c_methods as $m) {            
                 if(substr($m, 0, $l) === $prefix) $this->$m();
+            }
+        }
+    }
+
+    /**
+     * Call environment method if exists
+     * e.g. envDev() envProd() envStating() envTesting()
+     */
+    private function _env()
+    {
+        if(defined("APPLICATION_ENV")) {
+            $env_method = 'env'.APPLICATION_ENV;
+            if(method_exists($this, $env_method)) {
+                $this->$env_method();
             }
         }
     }
