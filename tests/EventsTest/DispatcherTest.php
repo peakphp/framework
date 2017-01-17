39c98bd25f99ed443805e4fdb9940b56516fbf91
Francois Lajoie <snake386@hotmail.com>
2017-01-16 23:07:13 -0500

    updated

--- tests/EventsTest/DispatcherTest.php
+++ tests/EventsTest/DispatcherTest.php
@@ -10,13 +10,50 @@ class DispatcherTest extends TestCase
     {   
         $d = new Dispatcher();  
 
-        $d->attach('myevent', function($argv, $data) {
-            print_r($argv);
-            print_r($data);
-            //echo 'hello'.$config->get('db1.driver').'<br>';
+        $d->attach('myevent', function($argv) {
+            //do something
         });
 
         $this->assertTrue($d->hasEvent('myevent'));
         $this->assertFalse($d->hasEvent('mysecondevent'));
+    }
+
+    public function testFire()
+    {   
+        $d = new Dispatcher();  
+
+        $var = 'barfoo';
+
+        $this->assertTrue($var === 'barfoo');
+
+        $d->attach('myevent', function($argv) use(&$var) {
+            $var = $argv;
+        });
+
+        $d->fire('myevent', 'foobar');
+
+        $this->assertTrue($var === 'foobar');
+    }
+
+    public function testMultipleFire()
+    {   
+        $d = new Dispatcher();  
+
+        $int = 0;
+
+        $d->attach('myevent', function() use(&$int) {
+            ++$int;
+        });
+        $d->attach('myevent', function() use(&$int) {
+            ++$int;
+        });
+        $d->attach('myevent', function() use(&$int) {
+            ++$int;
+        });
+
+
+        $d->fire('myevent');
+
+        $this->assertTrue($int == 3);
     }
 }
