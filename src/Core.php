03ed67870d9e707fec319eb30c3cd732a846144d
Francois Lajoie <snake386@hotmail.com>
2017-01-26 15:05:03 -0500

    added relativePath and renamed relative_basepath to camelstyle

--- src/Core.php
+++ src/Core.php
@@ -7,9 +7,9 @@ if(!defined('PEAK_VERSION'))
     define('PEAK_VERSION', '2.0.0');
 
 /**
- * relative_basepath()
+ * relativeBasepath()
  */
-if(!function_exists('relative_basepath')) {
+if(!function_exists('relativeBasepath')) {
     /**
      * Get relativepath of specified dir from the server document root
      * 
@@ -21,6 +21,24 @@ if(!function_exists('relative_basepath')) {
             $doc_root = (!isset($_SERVER['DOCUMENT_ROOT'])) ? '' : $_SERVER['DOCUMENT_ROOT'];
         }
         return substr(str_replace([$doc_root,basename($dir)],'',str_replace('\\','/',$dir)), 0, -1);
+    }
+}
+
+/**
+ * relativePath()
+ */
+if(!function_exists('relativePath')) {
+    /**
+     * Get relative path of specified dir from the server document root
+     * 
+     * @param  string $dir
+     * @return string     
+     */
+    function relativePath($dir, $doc_root = null) {
+        if(!isset($doc_root)) {
+            $doc_root = (!isset($_SERVER['DOCUMENT_ROOT'])) ? '' : $_SERVER['DOCUMENT_ROOT'];
+        }
+        return substr(str_replace([$doc_root,$dir],'',str_replace('\\','/',$dir)), 0, -1);
     }
 }
 
