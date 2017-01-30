fe84ca792ec8290b2b0ab59c5aee0235908755df
Francois Lajoie <snake386@hotmail.com>
2017-01-29 22:29:29 -0500

    added

---
+++ src/Config/Session.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Peak\Config;
+
+use Peak\Config\DotNotation;
+
+/**
+ * Wrap Collection / DotNotation abilities around php $_SESSION
+ */
+class Session extends DotNotation 
+{
+    /**
+     * We pass $_SESSION by reference so we can alter
+     * it through the collection
+     */
+    public function __construct()
+    {
+        $this->items =& $_SESSION;
+    }
+}
