8cb4b6436327afafc2547bf2214b5d1aba0af316
Francois Lajoie <snake386@hotmail.com>
2017-02-02 22:26:54 -0500

    updated comment

--- src/Config/DotNotation.php
+++ src/Config/DotNotation.php
@@ -6,12 +6,14 @@ use Peak\Collection;
 /**
  * Dot notation for access multidimensional arrays.
  * 
- * $dn = new DotNotation(['bar'=>['baz'=>['foo'=>true]]]);
+ * $dn = new DotNotation([
+ *     'bar'=> [
+ *         'baz'=> ['foo' => true]
+ *      ]
+ * ]);
  * 
- * $value = $dn->get('bar.baz.foo'); // $value == true
- * 
- * $dn->set('bar.baz.foo', false); // ['foo'=>false]
- * 
+ * $value = $dn->get('bar.baz.foo');   // $value == true
+ * $dn->set('bar.baz.foo', false);     // ['foo'=>false]
  * $dn->add('bar.baz', ['boo'=>true]); // ['foo'=>false,'boo'=>true]
  * 
  * @author Anton Medvedev <anton (at) elfet (dot) ru>
@@ -26,6 +28,8 @@ class DotNotation extends Collection
     const SEPARATOR = '/[:\.]/';
 
     /**
+     * Return a path value
+     * 
      * @param string $path
      * @param string $default
      * @return mixed
@@ -49,6 +53,8 @@ class DotNotation extends Collection
     }
 
     /**
+     * Add a path
+     * 
      * @param string $path
      * @param mixed $value
      */
@@ -81,7 +87,9 @@ class DotNotation extends Collection
     }
 
     /**
-     * @param $path
+     * Merge a path with an array
+     * 
+     * @param string $path
      * @param array $values
      */
     public function add($path, array $values)
@@ -91,6 +99,8 @@ class DotNotation extends Collection
     }
 
     /**
+     * Check if we have path
+     * 
      * @param string $path
      * @return bool
      */
@@ -105,66 +115,16 @@ class DotNotation extends Collection
                 return false;
             }
         }
-
         return true;
     }
 
-
+    /**
+     * Explode path string
+     * 
+     * @param  string $path    
+     */
     protected function explode($path)
     {
         return preg_split(self::SEPARATOR, $path);
-    }
-
-    /**
-     * array_merge_recursive does indeed merge arrays, but it converts values with duplicate
-     * keys to arrays rather than overwriting the value in the first array with the duplicate
-     * value in the second array, as array_merge does. I.e., with array_merge_recursive,
-     * this happens (documented behavior):
-     *
-     * array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
-     *     => array('key' => array('org value', 'new value'));
-     *
-     * arrayMergeRecursiveDistinct does not change the datatypes of the values in the arrays.
-     * Matching keys' values in the second array overwrite those in the first array, as is the
-     * case with array_merge, i.e.:
-     *
-     * arrayMergeRecursiveDistinct(array('key' => 'org value'), array('key' => 'new value'));
-     *     => array('key' => array('new value'));
-     *
-     * Parameters are passed by reference, though only for performance reasons. They're not
-     * altered by this function.
-     *
-     * If key is integer, it will be merged like array_merge do:
-     * arrayMergeRecursiveDistinct(array(0 => 'org value'), array(0 => 'new value'));
-     *     => array(0 => 'org value', 1 => 'new value');
-     *
-     * @param array $array1
-     * @param array $array2
-     * @return array
-     * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
-     * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
-     * @author Anton Medvedev <anton (at) elfet (dot) ru>
-     */
-    protected function arrayMergeRecursiveDistinct(array &$array1, array &$array2)
-    {
-        $merged = $array1;
-
-        foreach ($array2 as $key => &$value) {
-            if (is_array($value) && isset ($merged[$key]) && is_array($merged[$key])) {
-                if (is_int($key)) {
-                    $merged[] = $this->arrayMergeRecursiveDistinct($merged[$key], $value);
-                } else {
-                    $merged[$key] = $this->arrayMergeRecursiveDistinct($merged[$key], $value);
-                }
-            } else {
-                if (is_int($key)) {
-                    $merged[] = $value;
-                } else {
-                    $merged[$key] = $value;
-                }
-            }
-        }
-
-        return $merged;
     }
 }
