c2a0906ceb19912632f84995c2578b6568268249
Francois Lajoie <snake386@hotmail.com>
2017-02-04 22:58:06 -0500

    updated

--- tests/fixtures/config/appconf_example.php
+++ tests/fixtures/config/appconf_example.php
@@ -1,41 +1,38 @@
 <?php
 /**
- * Application configuration example in php instead of ini file
- * Faster to load but more complex to modify if we compare with app ini config file
- *
- * @version $Id$
+ * Config file example
  */
-return array(
-    'all' => array(
-            'php' => array(
-                    'display_errors' => 1,
-                    'display_startup_errors' => 1,
-                    'date' => array(
-                            'timezone' => 'America/Montreal'
-                        )
-                ),
+return [
+    'all' => [
+        'php' => [
+            'display_errors' => 1,
+            'display_startup_errors' => 1,
+            'date' => [
+                'timezone' => 'America/Montreal'
+            ]
+        ],
 
-            'front' => array(
-                    'allow_internal_controllers' => 1,
-                    'default_controller' => 'index',
-                    'error_controller' => 'error',
-                ),
+        'front' => [
+            'allow_internal_controllers' => 1,
+            'default_controller' => 'index',
+            'error_controller' => 'error',
+        ],
 
-            'view' => array(
-                    'engine' => 'Layouts',
-                    'useLayout' => 'homepage',
-                ),
+        'view' => [
+            'engine' => 'Layouts',
+            'useLayout' => 'homepage',
+        ],
 
-        ),
+    ],
 
-    'development' => array(),
-    'testing' => array(),
-    'staging' => array(),
+    'dev' => [],
+    'testing' => [],
+    'staging' => [],
 
-    'production' => array(
-            'php' => array(
-                    'display_errors' => 0,
-                    'display_startup_errors' => 0
-                ),
-        ),
-);
+    'prod' => [
+        'php' => [
+            'display_errors' => 0,
+            'display_startup_errors' => 0
+        ],
+    ],
+];
