9f1f0fab27cb2869f90f8f5d640deb41c9679411
Francois Lajoie <snake386@hotmail.com>
2017-02-06 21:56:16 -0500

    added explicit resolver

--- src/Di/ClassResolver.php
+++ src/Di/ClassResolver.php
@@ -2,9 +2,13 @@
 
 namespace Peak\Di;
 
+use Peak\Exception;
 use Peak\Di\Container;
 use Peak\Di\ClassInspector;
 use Peak\Di\InterfaceResolver;
+use Peak\Di\ExplicitResolver;
+
+use Closure;
 
 /**
  * Dependency Class Resolver
@@ -29,6 +33,7 @@ class ClassResolver
     public function __construct()
     {
         $this->inspector = new ClassInspector();
+        $this->explicit = new ExplicitResolver();
         $this->iresolver = new InterfaceResolver();
     }
 
@@ -51,21 +56,28 @@ class ClassResolver
                 throw new \Exception($d['error']);
             }
 
+            // its a class or an interface
             if(isset($d['class'])) {
 
                 $name = $d['class'];
                 ++$class_count;
 
-                if($container->hasInstance($name)) {
+                // look for object in explicit dependency declaration
+                $result = $this->explicit->resolve($name, $explicit);
+                if($result !== null) {
+                    $class_args[] = $result;
+                }
+                // check if container has a stored instance
+                else if($container->hasInstance($name)) {
                     $class_args[] = $container->getInstance($name);
                 }
                 else {
-
+                    // otherwise check if we are
                     // dealing with an interface dependency
                     if(interface_exists($name)) {
                         $class_args[] = $this->iresolver->resolve($name, $container, $explicit);
                     }
-                    // or resolve dependency by instanciate object classname
+                    // or resolve dependency by trying to instanciate object classname string
                     else {
                         $child_args = [];
                         if(array_key_exists($name, $args)) {
@@ -75,6 +87,7 @@ class ClassResolver
                     }
                 }
             }
+            // everything else that is not a type of class or interface
             else if(array_key_exists($i - ($class_count), $args)) {
                 $class_args[] = $args[$i - $class_count];
             }
---
+++ src/Di/ExplicitResolver.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Peak\Di;
+
+use Peak\Di\Container;
+use Peak\Exception;
+
+use Closure;
+
+/**
+ * Explicit Dependency declaration Resolver
+ */
+class ExplicitResolver
+{
+    /**
+     * Resolve class arguments dependencies
+     * 
+     * @param  string $class
+     * @return object
+     */
+    public function resolve($needle, $explicit = [])
+    {
+        // Check for explicit dependency declaration closure or object
+        if(array_key_exists($needle, $explicit)) {
+            if($explicit[$needle] instanceof Closure) {
+                return $explicit[$needle]();
+            }
+            elseif(is_object($explicit[$needle])) {
+                return $explicit[$needle];
+            }
+        }
+        return null;
+    }
+}
