7510218a02da0b3fc4428cfa1fdb291dcc3eb908
Francois Lajoie <snake386@hotmail.com>
2017-02-23 21:18:03 -0500

    added

---
+++ src/Validation/Rules/Int.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Peak\Validation\Rules;
+
+use Peak\Validation\AbstractRule;
+
+/**
+ * Integer rule using FILTER_VALIDATE_INT
+ */
+class Int extends AbstractRule
+{
+    /**
+     * Default options
+     * @var array
+     */
+    protected $default_options = [
+        'min_range' => null,
+        'max_range' => null,
+    ];
+
+    /**
+     * Validate
+     * 
+     * @param  mixed $value
+     * @return bool
+     */
+    public function validate($value)
+    {
+        $options = $this->getFilterVarOptions();
+        if(filter_var($value, FILTER_VALIDATE_INT, $options) !== false) {
+            return true;
+        }
+        return false;
+    }
+}
