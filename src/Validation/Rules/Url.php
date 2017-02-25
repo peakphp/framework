749c1108fefd89e6022a6adb8957ad838e1c910c
Francois Lajoie <snake386@hotmail.com>
2017-02-24 23:09:25 -0500

    added url rule

---
+++ src/Validation/Rules/Url.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Peak\Validation\Rules;
+
+use Peak\Validation\AbstractRule;
+
+/**
+ * Url rule using FILTER_VALIDATE_URL
+ */
+class Url extends AbstractRule
+{
+    /**
+     * Default options
+     * @var array
+     */
+    protected $default_options = [];
+
+    /**
+     * Validate
+     * 
+     * @param  mixed $value
+     * @return bool
+     */
+    public function validate($value)
+    {
+        $options = $this->getFilterVarOptions();
+        if(filter_var($value, FILTER_VALIDATE_URL, $options) !== false) {
+            return true;
+        }
+        return false;
+    }
+}
