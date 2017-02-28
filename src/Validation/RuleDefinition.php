a5edc4631a628a80353123872f602641c4ea0201
Francois Lajoie <snake386@hotmail.com>
2017-02-28 00:08:44 -0500

    added

---
+++ src/Validation/RuleDefinition.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Peak\Validation;
+
+class RuleDefinition
+{
+    /**
+     * Default options
+     * @var array
+     */
+    protected $default_def = [
+        'rule'    => null,
+        'options' => null,
+        'flags'   => null,
+        'error'   => null,
+    ];
+
+    /**
+     * Final rule definitions
+     * @var array
+     */
+    protected $def = [];
+
+    /**
+     * Construct
+     * 
+     * @param array $def 
+     */
+    public function __construct($def = null)
+    {
+        $this->def = $this->default_def;
+
+        if(is_array($def)) {
+            $this->def = array_merge($this->default_def, $def);
+        }
+    }
+
+    /**
+     * Get an defition by key
+     *
+     * @param  string $key
+     * @return mixed
+     */
+    public function &__get ($key) 
+    {
+        return $this->def[$key];
+    }
+}
