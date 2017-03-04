33211be7b586168d7498dd3937ecf756fe972209
Francois Lajoie <snake386@hotmail.com>
2017-03-04 15:12:44 -0500

    PSR2 update

--- src/Di/ExplicitResolver.php
+++ src/Di/ExplicitResolver.php
@@ -14,18 +14,18 @@ class ExplicitResolver
 {
     /**
      * Resolve class arguments dependencies
-     * 
+     *
      * @param  string $class
      * @return object
      */
     public function resolve($needle, $explicit = [])
     {
         // Check for explicit dependency closure or object instance
-        if(array_key_exists($needle, $explicit)) {
-            if($explicit[$needle] instanceof Closure) {
+        if (array_key_exists($needle, $explicit)) {
+            if ($explicit[$needle] instanceof Closure) {
                 return $explicit[$needle]();
             }
-            elseif(is_object($explicit[$needle])) {
+            elseif (is_object($explicit[$needle])) {
                 return $explicit[$needle];
             }
         }
