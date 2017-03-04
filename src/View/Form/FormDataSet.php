0a5e6042412728c365ff4a6b00803a9eb5502746
Francois Lajoie <snake386@hotmail.com>
2017-03-03 23:09:41 -0500

    added Validation wrapper around FormBuilder

---
+++ src/View/Form/FormDataSet.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Peak\View\Form;
+
+use Peak\Validation\DataSet;
+
+/**
+ * Wrap DataSet for FormValidation
+ */
+class FormDataSet extends DataSet
+{
+    public function setUp()
+    {
+    }
+}
