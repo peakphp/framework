a0f7218c0470fa8bc25096ca0f00cff71c9f8edb
Francois Lajoie <snake386@hotmail.com>
2017-03-04 15:12:38 -0500

    PSR2 update

--- src/Di/InterfaceResolver.php
+++ src/Di/InterfaceResolver.php
@@ -19,20 +19,19 @@ class InterfaceResolver
     public function resolve($interface, Container $container, $explicit = [])
     {
         // Try to find a match in the container for a class or an interface
-        if($container->hasInterface($interface)) {
+        if ($container->hasInterface($interface)) {
 
             $instance = $container->getInterface($interface);
 
-            if(is_array($instance)) {
+            if (is_array($instance)) {
 
-                if(empty($explicit) || !array_key_exists($interface, $explicit)) {
+                if (empty($explicit) || !array_key_exists($interface, $explicit)) {
                     throw new Exception ('Dependecies for interface '.$interface.' is ambiguous. There is '.count($instance).' differents instances for this interface.');
                 }
                 return $container->getInstance($explicit[$interface]);
             }
-            else {
-                return $container->getInstance($instance);
-            }
+            
+            return $container->getInstance($instance);
         }
         else {
             throw new Exception('Could not find an instance that implement interface '.$interface);
