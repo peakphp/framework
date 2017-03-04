6a03beb7ff251cf864b5ee019f9c93adccc56369
Francois Lajoie <snake386@hotmail.com>
2017-03-04 13:40:05 -0500

    PSR2 update

--- src/Paginator.php
+++ src/Paginator.php
@@ -27,7 +27,7 @@ class Paginator implements IteratorAggregate
   
     /**
      * Constructor
-     * 
+     *
      * @param integer  $items_per_page 
      * @param integer  $items_count    
      * @param integer  $current_page   
@@ -41,14 +41,14 @@ class Paginator implements IteratorAggregate
 
         $this->calculate();
 
-        if(isset($range)) {
+        if (isset($range)) {
             $this->setPagesRange($range);
         }
     }
 
     /**
      * Get $pages array
-     * 
+     *
      * @return ArrayIterator
      */
     public function getIterator()
@@ -58,7 +58,8 @@ class Paginator implements IteratorAggregate
 
     /**
      * Set page and (re)calculate
-     * @param $this
+     *
+     * @param integer $page
      */
     public function setPage($page)
     {
@@ -74,15 +75,15 @@ class Paginator implements IteratorAggregate
     {
         
         // calculate how many page
-        if($this->items_count > 0 && $this->items_per_page > 0) {
+        if ($this->items_count > 0 && $this->items_per_page > 0) {
             $this->pages_count = ceil(($this->items_count / $this->items_per_page));
         }
-        elseif($this->items_count == 0) $this->pages_count = 0;
+        elseif ($this->items_count == 0) $this->pages_count = 0;
         else $this->pages_count = 1;
         
         // generate pages array
         $this->pages = [];
-        if($this->pages_count < 1) $this->pages = [];
+        if ($this->pages_count < 1) $this->pages = [];
         else {
             $this->pages = range(1, $this->pages_count);
         }
@@ -91,10 +92,10 @@ class Paginator implements IteratorAggregate
         $this->last_page = ($this->pages_count < 1) ? null : $this->pages_count;
 
         // check current page
-        if(!$this->isPage($this->current_page)) {
+        if (!$this->isPage($this->current_page)) {
             trigger_error(__CLASS__.': page '.$this->current_page.' doesn\'t exists', E_USER_NOTICE);
-            if($this->current_page > $this->pages_count) $this->current_page = $this->pages_count;
-            elseif($this->current_page < 1) $this->current_page = 1;
+            if ($this->current_page > $this->pages_count) $this->current_page = $this->pages_count;
+            elseif ($this->current_page < 1) $this->current_page = 1;
             else $this->current_page = 1;
         }
         
@@ -106,8 +107,8 @@ class Paginator implements IteratorAggregate
         $this->item_start = (($this->current_page - 1) * $this->items_per_page);
         $this->item_end = $this->item_start + $this->items_per_page;
         
-        if(($this->items_count != 0)) ++$this->item_start;
-        if($this->item_end > $this->items_count) $this->item_end = $this->items_count;
+        if (($this->items_count != 0)) ++$this->item_start;
+        if ($this->item_end > $this->items_count) $this->item_end = $this->items_count;
 
         // item start offset
         $this->offset = $this->item_start - 1;
@@ -115,7 +116,6 @@ class Paginator implements IteratorAggregate
         return $this;
     }
 
-    
     /**
      * Set a pages ranges list array
      * Range represent the number of page before and after the current page
@@ -125,18 +125,18 @@ class Paginator implements IteratorAggregate
      */
     public function setPagesRange($range = null)
     {
-        if(is_numeric($range) && ($range <= $this->pages_count) && is_array($this->pages)) {
+        if (is_numeric($range) && ($range <= $this->pages_count) && is_array($this->pages)) {
             
             $pages_range = [];
  
             $diff = $range - $range - $range;
               
-            for($i = $diff;$i <= $range;++$i) {
-                if($i < 0) $index = $this->current_page + $i;
-                elseif($i == 0) $index = $this->current_page;
+            for ($i = $diff;$i <= $range;++$i) {
+                if ($i < 0) $index = $this->current_page + $i;
+                elseif ($i == 0) $index = $this->current_page;
                 else $index = $this->current_page + $i;
                 
-                if(!in_array($index, $this->pages)) continue;
+                if (!in_array($index, $this->pages)) continue;
                 $pages_range[] = $index;
             }
 
