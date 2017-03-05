aec598c07eb7ff770f214ce047d6455c668a07e9
Francois Lajoie <snake386@hotmail.com>
2017-03-05 09:56:45 -0500

    PSR2 update

--- src/View/Render/Partials.php
+++ src/View/Render/Partials.php
@@ -13,27 +13,27 @@ use Peak\View\Render;
  * 
  */
 class Partials extends Render
-{   
-    
-    protected $_groups;      //available partials groups   
-    protected $_group;       //current partials group file to render   
+{
+
+    protected $_groups;      //available partials groups
+    protected $_group;       //current partials group file to render
     protected $_group_name;  //current partials group name
-   
-    
+
+
     /**
-     * Load Partials engine with groups alias 
+     * Load Partials engine with groups alias
      *
      * @param array $groups
      */
     public function __construct($groups = null)
     {
         $this->_groups = $groups;
-        if(isset($this->_groups['default'])) $this->useGroup('default');        
+        if (isset($this->_groups['default'])) $this->useGroup('default');
     }
     
     /**
      * Submit array of files or point to $groups array keyname for rendering
-     * 
+     *
      * @example useGroup( array('header.php','[CONTENT]','footer.php') )
      * @example useGroup('content_left') will push $this->options['layouts'][$layout] array to $this->layout
      *
@@ -41,11 +41,10 @@ class Partials extends Render
      */
     public function useGroup($group)
     {
-        if(is_array($group)) {
+        if (is_array($group)) {
             $this->_group = $group;
             $this->_group_name = 'custom';
-        }
-        elseif(isset($this->_groups[$group])) {
+        } elseif (isset($this->_groups[$group])) {
             $this->_group = $this->_groups[$group];
             $this->_group_name = $group;
         }
@@ -59,8 +58,7 @@ class Partials extends Render
     {
         $this->_group = null;
     }
-    
-    
+
     /**
      * Render view(s)
      *
@@ -69,12 +67,12 @@ class Partials extends Render
      * @return array/string
      */
     public function render($file, $path = null)
-    {    
-        //CONTROLLER FILE VIEW       
+    {
+        //CONTROLLER FILE VIEW
         $this->_scripts_file = $file;
         $this->_scripts_path = $path;
         
-        if(!isset($path)) {
+        if (!isset($path)) {
             $this->scripts_path = Application::conf('path.apptree.theme_partials');
             $path = Application::conf('path.apptree.theme_partials');
             $no_cache = true;
@@ -82,37 +80,38 @@ class Partials extends Render
         
         $filepath = $path.'/'.$file;
 
-        if(!file_exists($filepath)) {
+        if (!file_exists($filepath)) {
             $filepath = Peak_Registry::o()->app->front->controller->title .'/'. basename($filepath);
-            throw new Peak_View_Exception('ERR_VIEW_SCRIPT_NOT_FOUND', $filepath);        
+            throw new Peak_View_Exception('ERR_VIEW_SCRIPT_NOT_FOUND', $filepath);
         }
-        
+
         //Partials group FILES VIEW IF EXISTS
-        if(is_array($this->_group))
-        {          
+        if (is_array($this->_group)) {
             $group_filespath = array();
             
-            foreach($this->_group as $theme_partial) {
-                if($theme_partial !== '[CONTENT]') {
-                    if(basename($theme_partial) === $theme_partial) {
-                        if(file_exists(Application::conf('path.apptree.theme_partials').'/'.$theme_partial)) {
+            foreach ($this->_group as $theme_partial) {
+                if ($theme_partial !== '[CONTENT]') {
+                    if (basename($theme_partial) === $theme_partial) {
+                        if (file_exists(Application::conf('path.apptree.theme_partials').'/'.$theme_partial)) {
                             $group_filespath[] = Application::conf('path.theme_partials').'/'.$theme_partial;
                         }
+                    } elseif (file_exists($theme_partial)) {
+                        $group_filespath[] = $theme_partial;
                     }
-                    elseif(file_exists($theme_partial)) $group_filespath[] = $theme_partial;
+                } else {
+                    $group_filespath[] = $filepath;
                 }
-                else $group_filespath[] = $filepath;
             }
-            
-            $this->preOutput($group_filespath);  
-            
-        }
-        else {
-            if(isset($no_cache)) $this->output($filepath);
-            else $this->preOutput($filepath);
+            $this->preOutput($group_filespath);
+        } else {
+            if (isset($no_cache)) 
+                $this->output($filepath);
+            } else {
+                $this->preOutput($filepath);
+            }
         }
     }
-    
+
     protected function output($viewfiles)
     {
         // remove partials group for Peak_View_Render_Partials
@@ -120,8 +119,13 @@ class Partials extends Render
         $this->noGroup();  
         
         // include controller action view with or without partials groups
-        if(is_array($viewfiles)) foreach($viewfiles as $file) include($file);
-        else include($viewfiles);    
+        if (is_array($viewfiles)) {
+            foreach($viewfiles as $file) {
+                include($file);
+            }
+        }
+        else {
+            include($viewfiles);
+        }
     }
-    
 }
