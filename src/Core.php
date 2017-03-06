4e006b40d899faf5a9c7894b30cc0c808641a119
Francois Lajoie <snake386@hotmail.com>
2017-03-05 19:39:16 -0500

    PSR2 update

--- src/Core.php
+++ src/Core.php
@@ -154,7 +154,7 @@ if (!function_exists('isEnv')) {
      * Check is env match curretn application env
      *
      * @param  string  $env
-     * @return boolean 
+     * @return boolean
      */
     function isEnv($env)
     {
@@ -189,8 +189,8 @@ if (!function_exists('collection')) {
     /**
      * Create a new Collection
      *
-     * @param  array|null $items 
-     * @return \Peak\Collection     
+     * @param  array|null $items
+     * @return \Peak\Collection
      */
     function collection($items = null)
     {
@@ -238,13 +238,13 @@ if (!function_exists('url')) {
     {
         $s = $_SERVER;
 
-        $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
-        $sp       = strtolower( $s['SERVER_PROTOCOL'] );
-        $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+        $ssl      = (!empty($s['HTTPS']) && $s['HTTPS'] == 'on');
+        $sp       = strtolower($s['SERVER_PROTOCOL']);
+        $protocol = substr($sp, 0, strpos($sp, '/')) . (( $ssl ) ? 's' : '');
         $port     = $s['SERVER_PORT'];
-        $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
-        $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
-        $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+        $port     = ((!$ssl && $port == '80') || ($ssl && $port=='443')) ? '' : ':'.$port;
+        $host     = ($use_forwarded_host && isset($s['HTTP_X_FORWARDED_HOST'])) ? $s['HTTP_X_FORWARDED_HOST'] : (isset($s['HTTP_HOST']) ? $s['HTTP_HOST'] : null);
+        $host     = isset($host) ? $host : $s['SERVER_NAME'] . $port;
 
         $final    = '//' . str_ireplace('//','/', $host.relativePath(config('path.public')).'/'.$path);
 
