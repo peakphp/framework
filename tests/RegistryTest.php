84c64e90e2929d223f320df80a024da4c82d5269
Francois Lajoie <snake386@hotmail.com>
2017-03-11 20:49:19 -0500

    updated

--- tests/RegistryTest.php
+++ tests/RegistryTest.php
@@ -2,7 +2,7 @@
 use PHPUnit\Framework\TestCase;
 
 /**
- * @package    Peak\Registry
+ * @package    Peak\Common\Registry
  */
 class RegistryTest extends TestCase
 {
@@ -15,73 +15,73 @@ class RegistryTest extends TestCase
     //clean object registered inside registry
     public static function tearDownAfterClass()
     {
-        $list = Peak\Registry::getObjectsList();
+        $list = Peak\Common\Registry::getObjectsList();
 
         if(!empty($list)) {
             foreach($list as $obj_name) {
-                Peak\Registry::unregister($obj_name);
+                Peak\Common\Registry::unregister($obj_name);
             }
         }
     }
     
     function testGetInstance()
     {
-        $reg = Peak\Registry::getInstance();        
-        $this->assertInstanceOf('Peak\Registry', $reg); 
+        $reg = Peak\Common\Registry::getInstance();        
+        $this->assertInstanceOf('Peak\Common\Registry', $reg); 
     }
     
     function testIsRegistered()
     {
-        $result = Peak\Registry::isRegistered('unregistered_object');       
+        $result = Peak\Common\Registry::isRegistered('unregistered_object');       
         $this->assertFalse($result);    
     }
     
     function testRegisteringObject()
     {
-        $obj = Peak\Registry::set('test_obj', new RegisteredClass());       
+        $obj = Peak\Common\Registry::set('test_obj', new RegisteredClass());       
         $this->assertInstanceOf('RegisteredClass',$obj);
-        $this->assertTrue(Peak\Registry::isRegistered('test_obj'),'test_obj should be registered');    
+        $this->assertTrue(Peak\Common\Registry::isRegistered('test_obj'),'test_obj should be registered');    
     }
     
     function testGetObject()
     {
-        $obj = Peak\Registry::get('test_obj');      
+        $obj = Peak\Common\Registry::get('test_obj');      
         $this->assertInstanceOf('RegisteredClass',$obj);
         
         unset($obj);        
-        $obj = Peak\Registry::o()->test_obj;        
+        $obj = Peak\Common\Registry::o()->test_obj;        
         $this->assertInstanceOf('RegisteredClass',$obj);
     }
     
     function testGetObjectsList()
     {
-        $list = Peak\Registry::getObjectsList();    
+        $list = Peak\Common\Registry::getObjectsList();    
         $this->assertTrue(is_array($list));                    
         $this->assertTrue(count($list) == 1);     
     }
     
     function testGetObjectClassname()
     {
-        $classname = Peak\Registry::getClassName('test_obj');       
+        $classname = Peak\Common\Registry::getClassName('test_obj');       
         $this->assertTrue($classname === 'RegisteredClass');
     }
     
     function testIsInstanceOf()
     {
-        $this->assertTrue(Peak\Registry::isInstanceOf('test_obj', 'RegisteredClass'));      
-        $this->assertFalse(Peak\Registry::isInstanceOf('test_obj', 'Unknowclass'));     
-        $this->assertFalse(Peak\Registry::isInstanceOf('test_obj2', 'RegisteredClass'));
+        $this->assertTrue(Peak\Common\Registry::isInstanceOf('test_obj', 'RegisteredClass'));      
+        $this->assertFalse(Peak\Common\Registry::isInstanceOf('test_obj', 'Unknowclass'));     
+        $this->assertFalse(Peak\Common\Registry::isInstanceOf('test_obj2', 'RegisteredClass'));
     }
     
     function testUnregisteringObject()
     {
-        Peak\Registry::unregister('test_obj');  
-        $this->assertFalse(Peak\Registry::isRegistered('test_obj'),'test_obj should be unregistered');
+        Peak\Common\Registry::unregister('test_obj');  
+        $this->assertFalse(Peak\Common\Registry::isRegistered('test_obj'),'test_obj should be unregistered');
     }
   
 }
 
-// example class that will be registered by Peak\Registry
+// example class that will be registered by Peak\Common\Registry
 class RegisteredClass
 {
     public function foo()
