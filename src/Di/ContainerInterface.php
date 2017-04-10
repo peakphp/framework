c58788201f1b327b993d4e6a3925896e0eaa9982
Francois Lajoie <snake386@hotmail.com>
2017-04-10 12:33:55 -0400

    added method get() and has()

--- src/Di/ContainerInterface.php
+++ src/Di/ContainerInterface.php
@@ -7,4 +7,18 @@ namespace Peak\Di;
  */
 interface ContainerInterface
 {
+    /**
+     * Has object instance
+     *
+     * @param  string $name
+     */
+    public function has($name);
+
+    /**
+     * Get an instance if exists, otherwise return null
+     *
+     * @param  string       $name
+     * @return object|null
+     */
+    public function get($name);
 }
