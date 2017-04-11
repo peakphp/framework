5972cd4b10496cc570fb6360f054b981d003f2c8
Francois Lajoie <snake386@hotmail.com>
2017-04-11 12:10:18 -0400

    renamed method hasInstance

--- src/Bedrock/Application.php
+++ src/Bedrock/Application.php
@@ -85,7 +85,7 @@ class Application
             throw new Exception(__CLASS__.' as no container');
         }
         if (isset($instance)) {
-            if (!self::$container->hasInstance($instance)) {
+            if (!self::$container->has($instance)) {
                 throw new Exception(__CLASS__.' container does not have '.$instance);
             }
         }
