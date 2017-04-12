3e9122e6f8693255e066b02dfa382a2d095741c7
Francois Lajoie <snake386@hotmail.com>
2017-04-11 21:02:33 -0400

    updated

--- tests/BedrockTest/ApplicationTest.php
+++ tests/BedrockTest/ApplicationTest.php
@@ -4,14 +4,32 @@ use PHPUnit\Framework\TestCase;
 class ApplicationTest extends TestCase
 {
     /**
-     * Create instance test
+     * test application container
      */
     function testApplicationContainer()
     {
         \Peak\Bedrock\Application::setContainer(new \Peak\Di\Container);
-        \Peak\Bedrock\Application::instantiate('\Peak\Bedrock\View');
 
         $this->assertTrue(\Peak\Bedrock\Application::container() instanceof \Peak\Di\ContainerInterface);
+    }
+
+    /**
+     * test application creation 
+     */
+    function testApplicationCreation()
+    {
+        $container = new \Peak\Di\Container;
+
+        $app = new \Peak\Bedrock\Application($container, [
+            'env'  => 'dev',
+            'conf' => 'config.php',
+            'path' => [
+                'public' => __DIR__,
+                'app'    => __DIR__.'/../fixtures/app/',
+            ]
+        ]);
+
+        $this->assertTrue($app instanceof \Peak\Bedrock\Application);
     }
     
 }
