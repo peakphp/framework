8610e61dbd0976ae3e6cbc5c9aeaef0db7e07052
Francois Lajoie <snake386@hotmail.com>
2017-04-13 22:45:54 -0400

    added Peak\Common\Language test

---
+++ tests/CommonTest/LanguageTest.php
@@ -0,0 +1,68 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class LanaguageTest extends TestCase
+{
+    /**
+     * Create object
+     */
+    public function testLanguageBasic()
+    {       
+        $lang = new \Peak\Common\Language('fr');
+
+        $this->assertTrue($lang->getLang() === 'fr');
+        $lang->setLang('en');
+        $this->assertTrue($lang->getLang() === 'en');
+    }
+
+    /**
+     * Add content
+     */
+    public function testAddContent()
+    {       
+        $lang = new \Peak\Common\Language('fr');
+
+        $translations = [
+            'error-file-not-found' => 'Fichier %s introuvable'
+        ];
+
+        $this->assertTrue($lang->getLang() === 'fr');
+        $lang->addContent($translations);
+        $this->assertTrue($lang->has('error-file-not-found'));
+        $this->assertFalse($lang->has('error-folder-not-found'));
+    }
+
+    /**
+     * Translate
+     */
+    public function testTranslate()
+    {       
+        $lang = new \Peak\Common\Language('fr');
+
+        $translations = [
+            'error-file-not-found' => 'Fichier %s introuvable'
+        ];
+
+        $lang->addContent($translations);
+        $this->assertTrue($lang->translate('error-file-not-found') === $translations['error-file-not-found']);
+        $this->assertTrue($lang->translate('error-file-not-found', ['myfile']) === 'Fichier myfile introuvable');
+
+        $this->assertTrue($lang->translate('unknow string...') === 'unknow string...');
+    }
+
+    /**
+     * Add files
+     */
+    public function testAddFiles()
+    {       
+        $lang = new \Peak\Common\Language('fr');
+
+        $lang->addFiles([
+            'arrayfile1.php',
+            'arrayfile2.php'
+        ], __DIR__.'/../fixtures/config');
+
+        $this->assertTrue($lang->translate('iam') === 'arrayfile2');
+    }   
+}
