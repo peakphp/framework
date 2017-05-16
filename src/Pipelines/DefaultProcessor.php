c3db99f8762f0cb242a624f8cb51264591ecc13b
Francois Lajoie <snake386@hotmail.com>
2017-05-16 11:22:04 -0400

    added comment

--- src/Pipelines/DefaultProcessor.php
+++ src/Pipelines/DefaultProcessor.php
@@ -7,6 +7,13 @@ use Peak\Pipelines\ProcessorInterface;
 
 class DefaultProcessor extends AbstractProcessor implements ProcessorInterface
 {
+    /**
+     * Process pipes
+     *
+     * @param  array $pipes
+     * @param  mixed $payload
+     * @return mixed
+     */
     public function process(array $pipes, $payload)
     {
         foreach ($pipes as $pipe) {
