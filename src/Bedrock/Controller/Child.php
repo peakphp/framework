37188d67c9a14addf8f0c37e1bfea8faab3ea4e0
Francois Lajoie <snake386@hotmail.com>
2017-05-19 12:19:03 -0400

    added

---
+++ src/Bedrock/Controller/Child.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Peak\Bedrock\Controller;
+
+use Peak\Bedrock\Application;
+use Peak\Bedrock\View;
+use Peak\Bedrock\Controller\ParentController;
+
+/**
+ * For standalone controller action class 
+ */
+abstract class Child
+{
+    /**
+     * View object
+     * @var Peak\Bedrock\View
+     */
+    protected $view;
+
+    /**
+     * Parent controller
+     * @var Peak\Bedrock\Controller\Parent
+     */
+    protected $parent;
+
+    /**
+     * Constructor
+     *
+     * @param View $view
+     * @param ParentController $parent
+     */
+    public function __construct(View $view, ParentController $parent)
+    {
+        $this->view = $view;
+        $this->parent = $parent;
+
+        // call child process() with di
+        Application::container()->call(
+            [$this, 'process']
+        );
+    }
+}
