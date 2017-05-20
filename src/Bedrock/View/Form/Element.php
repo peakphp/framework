a52d58c1cc0a96e7ec9b640dbbaac914253f3e73
Francois Lajoie <snake386@hotmail.com>
2017-05-20 16:57:59 -0400

    PSR-2 update

--- src/Bedrock/View/Form/Element.php
+++ src/Bedrock/View/Form/Element.php
@@ -151,7 +151,7 @@ abstract class Element implements ElementInterface
      * @param  string $string
      * @return boolean
      */
-    protected function isJson($string) 
+    protected function isJson($string)
     {
         json_decode($string);
         return (json_last_error() == JSON_ERROR_NONE);
