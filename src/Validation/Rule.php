5049c32c1ba5b84fff67aef5cebc71454468e5f5
Francois Lajoie <snake386@hotmail.com>
2017-05-24 12:02:55 -0400

    added __callStatic

--- src/Validation/Rule.php
+++ src/Validation/Rule.php
@@ -19,4 +19,29 @@ class Rule
     {
         return new RuleBuilder($name);
     }
+
+    /**
+     * Shorcut of create
+     * Rule::[ruleName]($value, [$options [, $flags [, $context ]]]);
+     *
+     * @param  string $method
+     * @param  array  $args
+     * @return boolean
+     */
+    public static function __callStatic($method, $args)
+    {
+        $rule = new RuleBuilder(ucfirst($method));
+
+        if (isset($args[1])) {
+            $rule->setOptions($args[1]);
+        }
+        if (isset($args[2])) {
+            $rule->setFlags($args[2]);
+        }
+        if (isset($args[3])) {
+            $rule->setContext($args[3]);
+        }
+
+        return $rule->validate($args[0]);
+    }
 }
