6ed88d6996bbc1491b936c9e024163c8e001b1a9
Francois Lajoie <snake386@hotmail.com>
2017-05-26 13:31:58 -0400

    moved test

--- tests/BedrockTest/ViewTest/FormTest/FormBuilderTest.php
+++ tests/BedrockTest/ViewTest/FormTest/FormBuilderTest.php
@@ -1,10 +1,11 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+use Peak\Bedrock\View\Form\Form;
 use Peak\Bedrock\View\Form\FormBuilder;
 use Peak\Bedrock\View\Form\FormValidation;
 
-include __DIR__.'/../../fixtures/view/form/FormBuilderExample.php';
+include __DIR__.'/../../../fixtures/view/form/FormBuilderExample.php';
 
 class FormBuilderTest extends TestCase
 {
@@ -14,7 +15,7 @@ class FormBuilderTest extends TestCase
      */
     function testValidateForm()
     {
-        $form_validation = new FormValidation(new FormBuilderExample());
+        $form_validation = new FormValidation(new FormBuilderExample(new Form()));
 
         $pass = $form_validation->validate([
             'id' => '',
@@ -29,7 +30,7 @@ class FormBuilderTest extends TestCase
      */
     function testValidateFormQuick()
     {
-        $form = new FormBuilderExample();
+        $form = new FormBuilderExample(new Form());
 
         $pass = $form->validate([
             'id' => '',
