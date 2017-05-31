35f58eab785380b786552a7f47c9da2bb1544fa9
Francois Lajoie <snake386@hotmail.com>
2017-05-31 10:58:05 -0400

    added

---
+++ src/Rbac/CustomPermission.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Peak\Rbac;
+
+use Peak\Rbac\Permission;
+
+class CustomPermission
+{
+    /**
+     * Allow flag
+     */
+    const ALLOW = true;
+
+    /**
+     * Deny flag
+     */
+    const DENY = false;
+
+    /**
+     * Permission instance
+     * @var Permission
+     */
+    protected $permission;
+
+    /**
+     * Permission allowed status
+     * @var bool
+     */
+    protected $allowed;
+
+    /**
+     * Constructor
+     *
+     * @param Permission $perm
+     * @param bool       $allowed
+     */
+    public function __construct(Permission $perm, bool $allowed)
+    {
+        $this->permission = $perm;
+        $this->allowed = $allowed;
+    }
+
+    /**
+     * Acces to permission object
+     *
+     * @param  string $method
+     * @param  array  $args
+     * @return mixed
+     */
+    public function __call($method, $args = null)
+    {
+        return call_user_func_array([$this->permission, $method], $args);
+    }
+
+    /**
+     * Is permission allowed
+     *
+     * @return boolean
+     */
+    public function isAllowed()
+    {
+        return $this->allowed;
+    }
+}
