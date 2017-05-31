292e0fd97e068f349bc4b52dfac12e5726176690
Francois Lajoie <snake386@hotmail.com>
2017-05-31 10:57:20 -0400

    updated

--- tests/RbacTest/ManagerTest.php
+++ tests/RbacTest/ManagerTest.php
@@ -6,6 +6,7 @@ use Peak\Rbac\Manager;
 use Peak\Rbac\User;
 use Peak\Rbac\Role;
 use Peak\Rbac\Permission;
+use Peak\Rbac\CustomPermission;
 
 class RbacManagerTest extends TestCase
 {
@@ -85,7 +86,7 @@ class RbacManagerTest extends TestCase
         $perm = $manager->createPermission('Play to soccer');
         $bob = $manager->createUser('Bob');
 
-        $adminrole = new Role('administrator');
+        $adminrole = $manager->createRole('administrator');
 
         $this->assertFalse($manager->user('Bob')->hasRole($adminrole));
         $this->assertFalse($manager->user('Bob')->hasRole('administrator'));
@@ -97,25 +98,126 @@ class RbacManagerTest extends TestCase
     }
 
     /**
-     * User can...
+     * User can with user
      */
     function testUserCan()
     {
         $manager = new Manager();
         
-        $perm = $manager->createPermission('Play to soccer');
-        $bob = $manager->createUser('Bob');
+        $perm1     = $manager->createPermission('Play to soccer');
+        $perm2     = $manager->createPermission('Touch the ball');
 
-        $adminrole = new Role('administrator');
+        $bob       = $manager->createUser('Bob');
+        $john      = $manager->createUser('John');
 
-        $this->assertFalse($bob->can($perm));
+        $adminrole = $manager->createRole('Administrator');
+        $userrole  = $manager->createRole('User');
+
+
+
+        $this->assertFalse($bob->can($perm1));
 
         $bob->addRole($adminrole);
 
-        $this->assertFalse($bob->can($perm));
+        $this->assertFalse($bob->can($perm1));
 
-        $perm->addRole($adminrole);
+        $perm1->addRole($adminrole);
+        $perm2->addRole($userrole);
+        $perm2->addRole($adminrole);
 
-        $this->assertTrue($bob->can($perm));
+        $this->assertTrue($bob->can($perm1));
+        $this->assertTrue($bob->can($perm2));
+
+        $john->addRole($userrole);
+
+        $this->assertFalse($john->can($perm1));
+        $this->assertTrue($john->can($perm2));
+
+
+    }
+
+    /**
+     * User can with manager
+     */
+    function testUserCan2()
+    {
+        $manager = new Manager();
+        
+        $perm1     = $manager->createPermission('Play to soccer');
+        $perm2     = $manager->createPermission('Touch the ball');
+
+        $bob       = $manager->createUser('Bob');
+        $adminrole = $manager->createRole('administrator');
+
+        $bob->addRole($adminrole);
+        $perm1->addRole($adminrole);
+
+        $result = $manager->userCan('Bob', [
+            'Play to soccer',
+            'Touch the ball',
+        ]);
+
+        $this->assertFalse($result);
+
+        $perm2->addRole($adminrole);
+
+        $result = $manager->userCan('Bob', [
+            'Play to soccer',
+            'Touch the ball',
+        ]);
+
+        $this->assertTrue($result);
+    }
+
+    /**
+     * User custom permission
+     */
+    function testUserCustomPermission()
+    {
+        $manager = new Manager();
+        
+        $perm1     = $manager->createPermission('Play to soccer');
+        $perm2     = $manager->createPermission('Touch the ball');
+
+        $bob       = $manager->createUser('Bob');
+        $adminrole = $manager->createRole('administrator');
+
+        $perm1->addRole($adminrole);
+        $bob->addRole($adminrole);
+
+        $this->assertTrue($bob->can($perm1));
+        $this->assertFalse($bob->can($perm2));
+
+        // bypass perm1
+        $custom_perm = new CustomPermission($perm1, CustomPermission::DENY);
+        $bob->addCustomPermission($custom_perm);
+
+        $this->assertFalse($bob->can($perm1));
+
+        // bypass perm2
+        $custom_perm = new CustomPermission($perm2, CustomPermission::ALLOW);
+        $bob->addCustomPermission($custom_perm);
+
+        $this->assertTrue($bob->can($perm2));
+
+        // with multiple perm
+        $result = $manager->userCan('Bob', [
+            'Play to soccer',
+            'Touch the ball',
+        ]);
+
+        $this->assertFalse($result);
+
+        // reallow first perm
+        $custom_perm = new CustomPermission($perm1, CustomPermission::ALLOW);
+        $bob->addCustomPermission($custom_perm);
+
+        // with multiple perm
+        $result = $manager->userCan('Bob', [
+            'Play to soccer',
+            'Touch the ball',
+        ]);
+
+        $this->assertTrue($result);
     }
 }
