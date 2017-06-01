e1176772f3ee6f778fe5621c76b9517e9d4cd723
Francois Lajoie <snake386@hotmail.com>
2017-06-01 17:07:49 -0400

    removed unused description functionality

--- src/Rbac/Permission.php
+++ src/Rbac/Permission.php
@@ -8,32 +8,4 @@ use Peak\Rbac\RolesHolder;
 class Permission extends AbstractHolder
 {
     use RolesHolder;
-    
-    /**
-     * Permission description
-     * @var string
-     */
-    protected $desc;
-
-    /**
-     * Overload parent constructor
-     *
-     * @param string $id   Permission identifier name
-     * @param string $desc Permission optionnal description
-     */
-    public function __construct($id, $desc = '')
-    {
-        $this->id = $id;
-        $this->desc = $desc;
-    }
-
-    /**
-     * Get permission description
-     *
-     * @return string
-     */
-    public function getDesc()
-    {
-        return $this->desc;
-    }
 }
