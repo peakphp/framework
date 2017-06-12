9f4db7acbca638eceb29a1295e81b52a539c3183
Francois Lajoie <snake386@hotmail.com>
2017-06-11 22:35:14 -0400

    added

---
+++ tests/ConfigTest/FileTest/IniTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use Peak\Config\File\Ini;
+
+class IniTest extends TestCase
+{
+    /**
+     * Test load a file
+     */
+    function testLoadFile()
+    {
+        $config = new Ini(__DIR__.'/../../fixtures/config/config.ini');
+
+        $this->assertTrue($config->have('all.php.display_errors'));
+        $this->assertTrue($config->get('all.php.display_errors') == 1);
+        $this->assertTrue(is_null($config->get('this.that')));
+    }
+
+    /**
+     * Test Load unknown file
+     */
+    function testException()
+    {
+        try {
+            $config =  new Ini('unknow.file.abc');
+        } catch (Exception $e) {
+
+        }
+
+        $this->assertFalse(isset($config));
+    }
+
+
+}
