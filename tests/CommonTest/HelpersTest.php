d4d9087a6716393e7b194601d86d4a3a909ef923
Francois Lajoie <snake386@hotmail.com>
2017-06-12 23:19:57 -0400

    added

---
+++ tests/CommonTest/HelpersTest.php
@@ -0,0 +1,66 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test helpers.php functions
+ */
+class HelpersTest extends TestCase
+{
+    /**
+     * test exceptionTrace()
+     */
+    function testExceptionTrace()
+    {
+        $content1 = exceptionTrace(new \Exception('Message'));
+        $this->assertTrue(mb_strlen($content1) > 1);
+
+        $content2 = exceptionTrace(new \Peak\Common\DataException('Message', ['misc_data']));
+        $this->assertTrue(mb_strlen($content2) > 1);
+        $this->assertTrue(mb_strlen($content2) > mb_strlen($content1));
+    }
+
+    /**
+     * test printExceptionTrace()
+     */
+    function testPrintExceptionTrace()
+    {
+        ob_start();
+        printExceptionTrace(new \Exception('Message'));
+        $content = ob_get_clean();
+        $this->assertTrue(mb_strlen($content) > 1);
+    }
+
+    /**
+     * test printHtmlExceptionTrace()
+     */
+    function testPrintHtmlExceptionTrace()
+    {
+        ob_start();
+        printHtmlExceptionTrace(new \Exception('Message'));
+        $content = ob_get_clean();
+        $this->assertTrue(mb_strlen($content) > 1);
+    }
+
+
+    /**
+     * test phpinput()
+     */
+    function testPhpinput()
+    {
+        $data = phpinput();
+        $this->assertTrue($data instanceof Peak\Common\Collection);
+    }
+
+    /**
+     * test phpShowAllErrors()
+     */
+    function testShowAllErrors()
+    {
+        ini_set('display_errors', 0);
+        $this->assertTrue(ini_get('display_errors') == 0);
+        showAllErrors();
+        $this->assertTrue(ini_get('display_errors') == 1);
+    }
+
+}
