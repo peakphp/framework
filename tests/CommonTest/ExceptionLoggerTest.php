5d93ddf7f38a0c75869e8b64cf9ea9c2fe31bdd6
Francois Lajoie <snake386@hotmail.com>
2017-06-12 22:26:38 -0400

    added

---
+++ tests/CommonTest/ExceptionLoggerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use Peak\Common\ExceptionLogger;
+
+class ExceptionLoggerTest extends TestCase
+{
+    /**
+     * test new instance
+     */
+    function testClass()
+    {
+        $file = __DIR__.'/error.log';
+        $el = new ExceptionLogger(
+            new \Exception('Message'),
+            $file
+        );
+
+        $this->assertTrue(file_exists($file));
+        unlink($file);
+    }
+
+
+}
