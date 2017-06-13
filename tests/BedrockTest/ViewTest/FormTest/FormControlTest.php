93fe6534b7fbcb91bf6d0222ad3c799939c05ee7
Francois Lajoie <snake386@hotmail.com>
2017-06-13 09:43:16 -0400

    updated

--- tests/BedrockTest/ViewTest/FormTest/FormControlTest.php
+++ tests/BedrockTest/ViewTest/FormTest/FormControlTest.php
@@ -2,6 +2,9 @@
 use PHPUnit\Framework\TestCase;
 
 use Peak\Bedrock\View\Form\Control\Input;
+use Peak\Bedrock\View\Form\Control\Checkbox;
+use Peak\Bedrock\View\Form\Control\Select;
+use Peak\Bedrock\View\Form\Control\Textarea;
 
 class FormControlTest extends TestCase
 {
@@ -13,14 +16,113 @@ class FormControlTest extends TestCase
     {
         $name = 'myinput';
         $data = 'test';
-        $options = [];
+        $options = [
+            'attrs' => [
+                'class' => 'form-control'
+            ]
+        ];
         $error = null;
 
-        $el = new Input($name, $data);
+        $el = new Input($name, $data, $options, $error);
         $el_html = $el->generate();
         $el_html2 = $el->get();
 
-        $result = '<input id="field-myinput" name="myinput" value="test" class="" placeholder="" spellcheck="true" type="text" ref="myinput">';
+        $result = '<input id="field-myinput" name="myinput" value="test" class="form-control" placeholder="" spellcheck="true" type="text" ref="myinput">';
+
+        $this->assertTrue($el_html === $result);
+        $this->assertTrue($el_html2 === $result);
+
+        ob_start();
+        $el->render();
+        $content = ob_get_clean();
+        $this->assertTrue($content === $result);
+    }
+
+    /**
+     * Test checkbox form control
+     */
+    function testCheckbox()
+    {
+        $name = 'myelement';
+        $data = 'test';
+        $options = [
+            'attrs' => [
+                'class' => 'form-control'
+            ]
+        ];
+        $error = null;
+
+        $el = new Checkbox($name, $data, $options, $error);
+        $el_html = $el->generate();
+        $el_html2 = $el->get();
+
+        //echo $el_html;
+
+        $result = '<input id="field-myelement" name="myelement" value="test" class="form-control" type="checkbox">';
+
+        $this->assertTrue($el_html === $result);
+        $this->assertTrue($el_html2 === $result);
+
+        ob_start();
+        $el->render();
+        $content = ob_get_clean();
+        $this->assertTrue($content === $result);
+    }
+
+    /**
+     * Test textarea form control
+     */
+    function testTextarea()
+    {
+        $name = 'myelement';
+        $data = 'test';
+        $options = [
+            'attrs' => [
+                'class' => 'form-control'
+            ]
+        ];
+        $error = null;
+
+        $el = new Textarea($name, $data, $options, $error);
+        $el_html = $el->generate();
+        $el_html2 = $el->get();
+
+        //echo $el_html;
+
+        $result = '<textarea id="field-myelement" name="myelement" class="form-control" placeholder="" row="3">test</textarea>';
+
+        $this->assertTrue($el_html === $result);
+        $this->assertTrue($el_html2 === $result);
+
+        ob_start();
+        $el->render();
+        $content = ob_get_clean();
+        $this->assertTrue($content === $result);
+    }
+
+    /**
+     * Test select form control
+     */
+    function testSelect()
+    {
+        $name = 'myelement';
+        $data = 'test';
+        $options = [
+            'options' => [
+                'a' => 'option a',
+                'b' => 'option b'
+            ],
+            'attrs' => [
+                'class' => 'form-control'
+            ]
+        ];
+        $error = null;
+
+        $el = new Select($name, $data, $options, $error);
+        $el_html = $el->generate();
+        $el_html2 = $el->get();
+
+        $result = '<select id="field-myelement" name="myelement" class="form-control" placeholder=""><option  value="a">option a</option><option  value="b">option b</option></select>';
 
         $this->assertTrue($el_html === $result);
         $this->assertTrue($el_html2 === $result);
