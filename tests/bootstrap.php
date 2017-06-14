511b3e9c0260178af24ed3fd23c5f256957e810c
Francois Lajoie <snake386@hotmail.com>
2017-06-14 06:58:58 -0400

    updated

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,19 @@
 include __DIR__.'/../vendor/autoload.php';
 
 define('FIXTURES_PATH', __DIR__.'/fixtures');
+
+/**
+ * Create a dummy app
+ */
+if(!function_exists('dummyApp')) {
+    function dummyApp($env = 'dev') {
+        return new Peak\Bedrock\Application(new Peak\Di\Container, [
+            'env'  => $env,
+            'conf' => 'config.php',
+            'path' => [
+                'public' => FIXTURES_PATH,
+                'app'    => FIXTURES_PATH.'/app/',
+            ]
+        ]);
+    }
+}
