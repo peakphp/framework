aec7e1f1d33df1aad784ef8a7aaa4088d03b8928
Francois Lajoie <snake386@hotmail.com>
2017-06-15 11:54:06 -0400

    updated http status codes

--- src/Bedrock/View/Header.php
+++ src/Bedrock/View/Header.php
@@ -38,7 +38,8 @@ class Header
     protected $http_status_codes = [
         100 => 'Continue',
         101 => 'Switching Protocols',
-        102 => 'Processing',
+        102 => 'Processing',                 // RFC 2518, obsoleted by RFC 4918
+
         200 => 'OK',
         201 => 'Created',
         202 => 'Accepted',
@@ -46,16 +47,19 @@ class Header
         204 => 'No Content',
         205 => 'Reset Content',
         206 => 'Partial Content',
-        207 => 'Multi-Status',
+        207 => 'Multi-Status',               // RFC 4918
+        208 => 'Already Reported',
+        226 => 'IM Used',
+
         300 => 'Multiple Choices',
         301 => 'Moved Permanently',
         302 => 'Found',
         303 => 'See Other',
         304 => 'Not Modified',
         305 => 'Use Proxy',
-        306 => '(Unused)',
         307 => 'Temporary Redirect',
-        308 => 'Permanent Redirect',
+        308 => 'Permanent Redirect',         // RFC 7238
+
         400 => 'Bad Request',
         401 => 'Unauthorized',
         402 => 'Payment Required',
@@ -69,46 +73,35 @@ class Header
         410 => 'Gone',
         411 => 'Length Required',
         412 => 'Precondition Failed',
-        413 => 'Request Entity Too Large',
-        414 => 'Request-URI Too Long',
+        413 => 'Payload Too Large',
+        414 => 'URI Too Long',
         415 => 'Unsupported Media Type',
-        416 => 'Requested Range Not Satisfiable',
+        416 => 'Range Not Satisfiable',
         417 => 'Expectation Failed',
-        418 => 'I\'m a teapot',
-        419 => 'Authentication Timeout',
-        420 => 'Enhance Your Calm',
-        422 => 'Unprocessable Entity',
-        423 => 'Locked',
-        424 => 'Failed Dependency',
-        424 => 'Method Failure',
-        425 => 'Unordered Collection',
-        426 => 'Upgrade Required',
-        428 => 'Precondition Required',
-        429 => 'Too Many Requests',
-        431 => 'Request Header Fields Too Large',
-        444 => 'No Response',
-        449 => 'Retry With',
-        450 => 'Blocked by Windows Parental Controls',
+        418 => 'I\'m a teapot',              // RFC 2324
+        421 => 'Misdirected Request',
+        422 => 'Unprocessable Entity',       // RFC 4918
+        423 => 'Locked',                     // RFC 4918
+        424 => 'Failed Dependency',          // RFC 4918
+        425 => 'Unordered Collection',       // RFC 4918
+        426 => 'Upgrade Required',           // RFC 2817
+        428 => 'Precondition Required',      // RFC 6585
+        429 => 'Too Many Requests',          // RFC 6585
+        431 => 'Request Header Fields Too Large',// RFC 6585
         451 => 'Unavailable For Legal Reasons',
-        494 => 'Request Header Too Large',
-        495 => 'Cert Error',
-        496 => 'No Cert',
-        497 => 'HTTP to HTTPS',
-        499 => 'Client Closed Request',
+        
         500 => 'Internal Server Error',
         501 => 'Not Implemented',
         502 => 'Bad Gateway',
         503 => 'Service Unavailable',
         504 => 'Gateway Timeout',
         505 => 'HTTP Version Not Supported',
-        506 => 'Variant Also Negotiates',
-        507 => 'Insufficient Storage',
+        506 => 'Variant Also Negotiates',    // RFC 2295
+        507 => 'Insufficient Storage',       // RFC 4918
         508 => 'Loop Detected',
         509 => 'Bandwidth Limit Exceeded',
-        510 => 'Not Extended',
-        511 => 'Network Authentication Required',
-        598 => 'Network read timeout error',
-        599 => 'Network connect timeout error'
+        510 => 'Not Extended',               // RFC 2774
+        511 => 'Network Authentication Required' // RFC 6585
     ];
 
     /**
