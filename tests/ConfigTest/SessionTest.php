b5d84dec7c1b14e6f1189ab59d6a1c011548c9f4
Francois Lajoie <snake386@hotmail.com>
2017-06-16 09:01:01 -0400

    added

---
+++ tests/ConfigTest/SessionTest.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+use Peak\Config\Session;
+
+class ConfigSessionTest extends TestCase
+{
+    function testCreateObject()
+    {
+        $config =  new Session();
+
+    }
+
+}
