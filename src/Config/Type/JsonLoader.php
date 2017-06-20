62d0bb67aaf36b877b1665231bcc918fb853a431
Francois Lajoie <snake386@hotmail.com>
2017-06-20 01:39:31 -0400

    added comment support

--- src/Config/Type/JsonLoader.php
+++ src/Config/Type/JsonLoader.php
@@ -18,6 +18,8 @@ class JsonLoader extends Loader
         if (file_exists($config)) {
             $config = file_get_contents($config);
         }
+        // remove comment(s) // and /* */
+        $config = preg_replace("#(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|([\s\t](//).*)#", '', $config);
         $this->content = json_decode($config, true);
         if (json_last_error() !== JSON_ERROR_NONE) {
             throw new Exception(__CLASS__.': error while decoding file ['.$file.'] > '.json_last_error_msg());
