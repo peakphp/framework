3b079dcf4eba3fbada9551e553d8f1b3b2448e84
Francois Lajoie <snake386@hotmail.com>
2017-07-04 20:47:50 -0400

    updated

--- tests/CommonTest/TimeExpressionTest.php
+++ tests/CommonTest/TimeExpressionTest.php
@@ -15,6 +15,8 @@ class TimeExpressionTest extends TestCase
         $this->assertTrue( (new TimeExpression("2d 15min"))->toSeconds() === 173700);
         $this->assertTrue( (new TimeExpression("15min2d"))->toSeconds() === 173700);
 
+        $this->assertTrue( (new TimeExpression("4min30sec"))->toSeconds() === 270);
+
         $this->assertTrue((new TimeExpression(86400 + 43200))->toString() === '1 day 12 hours');
         $this->assertTrue((new TimeExpression(86400 + 43199))->toString() === '1 day 11 hours 59 mins 59 secs');
         $this->assertTrue((new TimeExpression(1296000))->toString() === '15 days');
