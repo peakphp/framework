91a68213e2a8e36f820604087c28b4e215f0af5e
Francois Lajoie <snake386@hotmail.com>
2017-07-06 14:15:50 -0400

    moved logic to Peak\Climber/Cron/Cron

--- src/Climber/Cron/CronCommand.php
+++ src/Climber/Cron/CronCommand.php
@@ -7,7 +7,6 @@ use Peak\Climber\Cron\Exception\DatabaseNotFoundException;
 use Peak\Climber\Cron\Exception\TablesNotFoundException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use \Exception;
 
 abstract class CronCommand extends CommandWithDb
 {
@@ -19,27 +18,11 @@ abstract class CronCommand extends CommandWithDb
      */
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
-        // check for database connection
-        if (!isset($this->conn)) {
+        // run some validation for cron system
+        if (!Cron::hasDbConnection($this->conn)) {
             throw new DatabaseNotFoundException('No connection to a database has been found!');
-        } elseif (!$this->isInstalled() && $this->getName() !== 'cron:install') {
+        } elseif (!Cron::isInstalled($this->conn) && $this->getName() !== 'cron:install') {
             throw new TablesNotFoundException('Cron system is not installed. Please, use command cron:install before using cron commands');
-        }
-    }
-
-    /**
-     * Check if cron database installed
-     *
-     * @return mixed
-     */
-    public function isInstalled()
-    {
-        // check if table cron exists
-        try {
-            $this->conn->query("SELECT id FROM climber_cron LIMIT 1");
-            return true;
-        } catch(Exception $e) {
-            return false;
         }
     }
 }
