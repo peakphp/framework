6e81d69ff422c41a77a333c4bd541c7b62fa88de
Francois Lajoie <snake386@hotmail.com>
2017-07-06 14:15:24 -0400

    added

---
+++ src/Climber/Cron/Cron.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Peak\Climber\Cron;
+
+use Doctrine\DBAL\Connection;
+use \Exception;
+
+class Cron
+{
+    /**
+     * Check if ww have valid Doctrine connection instance
+     *
+     * @param Connection|null $conn
+     * @return bool
+     */
+    public static function hasDbConnection(Connection $conn = null)
+    {
+        return ($conn instanceof Connection);
+    }
+
+    /**
+     * Check if cron system is installed
+     *
+     * @param Connection|null $conn
+     * @return bool
+     */
+    public static function isInstalled(Connection $conn = null)
+    {
+        // check if table cron exists
+        try {
+            $conn->query("SELECT id FROM climber_cron LIMIT 1");
+            return true;
+        } catch(Exception $e) {
+            return false;
+        }
+    }
+}
