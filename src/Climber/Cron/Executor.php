67b2833b29eb032090f471f2eadc363b76e515c7
Francois Lajoie <snake386@hotmail.com>
2017-07-06 14:31:19 -0400

    updated

--- src/Climber/Cron/Executor.php
+++ src/Climber/Cron/Executor.php
@@ -4,18 +4,32 @@ namespace Peak\Climber\Cron;
 
 use Doctrine\DBAL\Connection;
 use Peak\Climber\Application;
-use Peak\Climber\Bootstrap\ConfigCronDb;
 use Peak\Climber\Cron\Exception\DatabaseNotFoundException;
 use Peak\Climber\Cron\Exception\TablesNotFoundException;
+use Peak\Di\ContainerInterface;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class Executor
 {
+    /**
+     * @var null|object
+     */
     protected $conn;
 
+    /**
+     * @var Application
+     */
     protected $app;
 
+    /**
+     * Constructor
+     *
+     * @param ContainerInterface|null $container
+     * @param array $config
+     * @throws DatabaseNotFoundException
+     * @throws TablesNotFoundException
+     */
     public function __construct(ContainerInterface $container = null, array $config = [])
     {
 
@@ -33,6 +47,9 @@ class Executor
         }
     }
 
+    /**
+     * Run cron job system
+     */
     public function run()
     {
 
