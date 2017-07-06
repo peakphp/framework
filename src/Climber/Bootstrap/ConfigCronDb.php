5a2cca1000e0b4945f8b63b1436da15ed84c1c2c
Francois Lajoie <snake386@hotmail.com>
2017-07-06 10:42:38 -0400

    moved checkInstall() logic to CronCommand

--- src/Climber/Bootstrap/ConfigCronDb.php
+++ src/Climber/Bootstrap/ConfigCronDb.php
@@ -6,9 +6,6 @@ use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\DriverManager;
 use Peak\Bedrock\Application\Config;
 use Peak\Climber\Application;
-use Peak\Climber\Cron\InstallDatabase;
-use \Exception;
-
 
 class ConfigCronDb
 {
@@ -29,21 +26,6 @@ class ConfigCronDb
 
         $this->conn = DriverManager::getConnection($conf->crondb, new Configuration());
 
-        Application::container()->add($this->conn, 'DbConnection');
-
-        $this->checkInstall();
-    }
-
-    /**
-     * Check if cron database installed
-     */
-    public function checkInstall()
-    {
-        // check if table cron exists
-        try {
-            $this->conn->query("SELECT id FROM climber_cron LIMIT 1");
-        } catch(Exception $e) {
-            new InstallDatabase($this->conn, Application::conf('crondb.driver'));
-        }
+        Application::container()->add($this->conn, 'CronDbConnection');
     }
 }
