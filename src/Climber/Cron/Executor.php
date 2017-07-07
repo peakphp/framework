fdbaf9cb8b186ec0b49d7dbd19f542b7c90d0ac9
Francois Lajoie <snake386@hotmail.com>
2017-07-06 21:38:40 -0400

    added run() and processCron()

--- src/Climber/Cron/Executor.php
+++ src/Climber/Cron/Executor.php
@@ -2,7 +2,6 @@
 
 namespace Peak\Climber\Cron;
 
-use Doctrine\DBAL\Connection;
 use Peak\Climber\Application;
 use Peak\Climber\Cron\Exception\DatabaseNotFoundException;
 use Peak\Climber\Cron\Exception\TablesNotFoundException;
@@ -52,6 +51,71 @@ class Executor
      */
     public function run()
     {
+        $qb = $this->conn->createQueryBuilder();
 
+        $qb->select('*')
+            ->from('climber_cron')
+            ->where('enabled = 1');
+
+        $result = $qb->execute();
+        $count = $result->rowCount();
+
+        $time = round(microtime(true));
+        //echo date('Y-m-d H:i:s', $time);
+        foreach ($result as $cron) {
+            if ($cron['repeat'] == -1 && empty($cron['last_execution'])) {
+                $this->processCron($cron);
+            } elseif ($cron['repeat'] == 0 && ( ($cron['interval'] + $time) <= $cron['last_execution'] ) ) {
+                $this->processCron($cron);
+            }
+        }
+    }
+
+    public function processCron($cron)
+    {
+        $climber_prefix = $this->app->conf('climber_cmd_prefix');
+
+        $update = [
+            'last_execution' => time(),
+            'error' => 0
+        ];
+
+        if ($cron['repeat'] > 1) {
+            --$cron['repeat'];
+            $update['next_execution'] = time() + $cron['interval'];
+        } elseif($cron['repeat'] == 1) {
+            $cron['repeat'] = -1;
+        }
+
+        if ($cron['repeat'] == -1) {
+            $update['enabled'] = 0;
+            $update['next_execution'] = null;
+        }
+
+        $cmd = $cron['cmd'];
+        if (!empty($climber_prefix) && $cron['sys_cmd'] == 0) {
+            $cmd = $climber_prefix.' '.$cmd;
+        }
+
+        echo 'Running cron job #'.$cron['id'].':'."\n";
+        echo '$ '.$cmd."\n";
+
+        $process = new Process($cmd);
+        $process->run();
+
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            $update['error'] = 1;
+        }
+
+        $this->conn->update('climber_cron', $update, [
+            'id' => $cron['id']
+        ]);
+//
+//        if (!$process->isSuccessful()) {
+//            throw new ProcessFailedException($process);
+//        }
+
+        echo $process->getOutput();
     }
 }
