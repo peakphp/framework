2f6cbb0112e673120359d1634e677ea35145b950
Francois Lajoie <snake386@hotmail.com>
2017-07-07 23:53:58 -0400

    added

---
+++ src/Climber/Commands/CronRunCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Peak\Climber\Commands;
+
+use Peak\Climber\Cron\CronCommand;
+use Peak\Climber\Cron\Executor;
+use Peak\Config\ConfigLoader;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use \Exception;
+
+class CronRunCommand extends CronCommand
+{
+    /**
+     * Configure command
+     */
+    protected function configure()
+    {
+        $this
+            // the name of the command (the part after "bin/console")
+            ->setName('cron:run')
+
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Run cron jobs')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This will run all cron jobs applicable at the moment')
+
+            ->setDefinition(
+                new InputDefinition([
+                    new InputArgument('config', InputArgument::REQUIRED, 'Configuration file path')
+                ])
+            );
+    }
+
+    /**
+     * Process command
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $file = $input->getArgument('config');
+
+        try {
+            $config = (new ConfigLoader([$file]))->asArray();
+        } catch (\Exception $e) {
+            return $output->writeln($e->getMessage());
+        }
+
+        try {
+            // create cli application for cron
+            (new Executor(null, $config))->run();
+        } catch(Exception $e) {
+            echo '['.get_class($e).']:'."\n".$e->getMessage();
+        }
+    }
+}
