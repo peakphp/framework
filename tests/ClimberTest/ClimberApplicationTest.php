b9b9f64c6c6d0365f3af48111bb7093675b4a06a
Francois Lajoie <snake386@hotmail.com>
2017-07-09 21:55:34 -0400

    updated

--- tests/ClimberTest/ClimberApplicationTest.php
+++ tests/ClimberTest/ClimberApplicationTest.php
@@ -1,23 +1,37 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
 use Peak\Climber\Application;
+use Peak\Climber\Commands\CronAddCommand;
+use Peak\Climber\Cron\RegisterCommands;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class ClimberApplicationTest extends TestCase
 {
 
     function testApplication()
     {
+        $application = new Application(null, [
+            'env' => 'dev',
+            'conf' => [
+                FIXTURES_PATH . '/config/cli.yml',
+                FIXTURES_PATH . '/config/cron.database.php',
+            ]
+        ]);
+
+        new RegisterCommands($application);
+
+        $container = Application::container();
+        $addcommand = $container->instantiate(CronAddCommand::class);
+
         try {
-            $application = new Application(null, [
-                'env' => 'dev',
-                'conf' => [
-                    FIXTURES_PATH . '/config/cli.yml'
-                ]
-            ]);
-        } catch (Exception $e) {
-            echo $e->getMessage();
+            $commandTester = new CommandTester($addcommand);
+            $commandTester->execute([]);
+        } catch(Exception $e) {
+            $error = true;
         }
+        $this->assertTrue(isset($error));
 
         $this->assertTrue($application instanceof Application);
         $this->assertTrue(Application::conf() instanceof \Peak\Bedrock\Application\Config);
