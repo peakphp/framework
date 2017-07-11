b65f1bb7ecc1cb4f1e4a416a6332c3d533580b3a
Francois Lajoie <snake386@hotmail.com>
2017-07-10 20:24:28 -0400

    updated

--- tests/ClimberTest/ClimberApplicationTest.php
+++ tests/ClimberTest/ClimberApplicationTest.php
@@ -3,36 +3,75 @@
 use PHPUnit\Framework\TestCase;
 
 use Peak\Climber\Application;
+use Peak\Bedrock\Application\Config;
 use Peak\Climber\Commands\CronAddCommand;
 use Peak\Climber\Cron\RegisterCommands;
+use Peak\Climber\Cron\Exception\InvalidDatabaseConfigException;
+use Peak\Climber\Cron\Exception\DatabaseNotFoundException;
+use Peak\Climber\Cron\Exception\TablesNotFoundException;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class ClimberApplicationTest extends TestCase
 {
-    function testApplication()
+
+    protected function appNoConnection()
     {
-        $application = new Application(null, [
+        return new Application(null, [
+            'env' => 'dev',
+            'conf' => [
+                FIXTURES_PATH . '/config/cli.yml',
+            ]
+        ]);
+    }
+
+    protected function appNoTables()
+    {
+        return new Application(null, [
             'env' => 'dev',
             'conf' => [
                 FIXTURES_PATH . '/config/cli.yml',
                 FIXTURES_PATH . '/config/cron.database.php',
             ]
         ]);
+    }
 
-        new RegisterCommands($application);
+    public function setup()
+    {
+//        $this->application = new Application(null, [
+//            'env' => 'dev',
+//            'conf' => [
+//                FIXTURES_PATH . '/config/cli.yml',
+//                FIXTURES_PATH . '/config/cron.database.php',
+//            ]
+//        ]);
+//
+//        $this->container = Application::container();
+    }
 
-        $container = Application::container();
-        $addcommand = $container->instantiate(CronAddCommand::class);
+    function testApplication()
+    {
+        $app = $this->appNoTables();
+        new RegisterCommands($app);
+
+        $this->assertTrue($app instanceof Application);
+        $this->assertTrue(Application::conf() instanceof Config);
+        $this->assertTrue(Application::conf('php.date.timezone') === "America/Toronto");
+    }
+
+    function testNoDatabaseConfiguration()
+    {
+        $app = $this->appNoConnection();
 
         try {
+            new RegisterCommands($app);
+            $addcommand = Application::container()->instantiate(CronAddCommand::class);
             $commandTester = new CommandTester($addcommand);
             $commandTester->execute([]);
         } catch(Exception $e) {
-            $error = true;
+            $error = $e;
         }
+
         $this->assertTrue(isset($error));
-        $this->assertTrue($application instanceof Application);
-        $this->assertTrue(Application::conf() instanceof \Peak\Bedrock\Application\Config);
-        $this->assertTrue(Application::conf('php.date.timezone') === "America/Toronto");
+        $this->assertTrue($error instanceof InvalidDatabaseConfigException);
     }
 }
