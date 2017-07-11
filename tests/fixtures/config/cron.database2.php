7210581b745f1d353fee8fcce6cc952621658d2b
Francois Lajoie <snake386@hotmail.com>
2017-07-11 10:03:17 -0400

    added

---
+++ tests/fixtures/config/cron.database2.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'crondb' => [
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'dbname' => 'improbable_database_name',
+        'user' => 'unknow_user',
+        'password' => 'unknow_password',
+    ],
+];
---
+++ tests/fixtures/config/cron.database3.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'crondb' => [
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'dbname' => 'improbable_database_name',
+        'user' => 'root',
+        'password' => '',
+    ],
+];
