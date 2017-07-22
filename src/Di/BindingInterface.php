8a5adcf8a7de25bf41c9916d19138813e5cb53bd
Francois Lajoie <snake386@hotmail.com>
2017-07-22 12:47:46 -0400

    added

---
+++ src/Di/BindingInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Peak\Di;
+
+use Psr\Container\ContainerInterface;
+
+interface BindingInterface
+{
+    public function getName();
+
+    public function getType();
+
+    public function getDefinition();
+
+    public function resolve(ContainerInterface $container, $args = [], $explicit = null);
+
+}
---
+++ src/Di/BindingResolver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Peak\Di;
+
+use Psr\Container\ContainerInterface;
+
+class BindingResolver
+{
+    /**
+     * Resolve a binding request
+     *
+     * @param BindingInterface $binding
+     * @param ContainerInterface $container
+     * @param array $args
+     * @param null $explicit
+     * @return mixed
+     */
+    public function resolve(BindingInterface $binding, ContainerInterface $container, $args = [], $explicit = null)
+    {
+        return $binding->resolve($container, $args, $explicit);
+    }
+
+}
