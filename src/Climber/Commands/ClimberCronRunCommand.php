4723fe24de6e852ec690ade7f4655076d9f2d8ee
Francois Lajoie <snake386@hotmail.com>
2017-08-13 13:10:33 -0400

    removed unused dependency

--- src/Climber/Commands/ClimberCronRunCommand.php
+++ src/Climber/Commands/ClimberCronRunCommand.php
@@ -8,7 +8,6 @@ use Peak\Config\ConfigLoader;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputDefinition;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use \Exception;
 
