6d6346799251fae3904e1608c5439e1e91246ad9
Francois Lajoie <snake386@hotmail.com>
2017-08-14 14:38:53 -0400

    updated

--- tests/CommonTest/TimeExpressionTest.php
+++ tests/CommonTest/TimeExpressionTest.php
@@ -19,6 +19,7 @@ class TimeExpressionTest extends TestCase
         $this->assertTrue( (new TimeExpression("2day 15min"))->toSeconds() === 173700);
         $this->assertTrue( (new TimeExpression("15min2day"))->toSeconds() === 173700);
         $this->assertTrue( (new TimeExpression("4min30sec"))->toSeconds() === 270);
+        $this->assertTrue( (new TimeExpression("4mins"))->toSeconds() === 240);
 
         // using DateInterval syntax (ISO8601 interval spec)
         $this->assertTrue( (new TimeExpression("PT1H35M45S"))->toSeconds() === 5745);
@@ -34,7 +35,6 @@ class TimeExpressionTest extends TestCase
             (new TimeExpression("P3DT5H"))->toString('%y years %d days %h hours') === '0 years 3 days 5 hours'
         );
 
-        //print_r((new TimeExpression(1000.154))->toString());
 
         // using TimeExpression to substract time from a DateTime
         $datetime = new DateTime('2017-08-14 11:00:00');
@@ -45,6 +45,45 @@ class TimeExpressionTest extends TestCase
         $datetime = new DateTime('2017-08-14 11:00:00');
         $datetime->add(new TimeExpression('PT25M15S'));
         $this->assertTrue($datetime->format('Y-m-d H:i:s') === '2017-08-14 11:25:15');
+
+        // using TimeExpression to add time from a DateTime
+        $datetime = new DateTime('2017-08-14 11:00:00');
+        $datetime->add(new TimeExpression('25 mins 15 secs'));
+        $this->assertTrue($datetime->format('Y-m-d H:i:s') === '2017-08-14 11:25:15');
+
+        // getting ISO8601 interval spec
+        $this->assertTrue( (new TimeExpression('2 days 1 sec'))->toIntervalSpec() === 'P2DT1S');
+
+//        echo "\n";
+//        echo (new TimeExpression(125))->toSeconds(); // 125
+//        echo "\n";
+//        echo (new TimeExpression("2day"))->toSeconds(); // 172800
+//        echo "\n";
+//        echo (new TimeExpression("4min30sec"))->toSeconds(); // 270;
+//        echo "\n";
+//
+//        // using DateInterval (ISO8601 interval spec)
+//        echo (new TimeExpression("PT1H35M45S"))->toSeconds(); //5745;
+//        echo "\n";
+//        echo (new TimeExpression("PT1H35M45S"))->toString(); //1 hour 35 minutes 45 seconds;
+//        echo "\n";
+//        echo (new TimeExpression("PT1H35M45S"))->toString('%y Years, %m Months, %d Days, %h Hours, %i Minutes, %s Seconds');
+//        //0 Years, 0 Months, 0 Days, 1 Hours, 35 Minutes, 45 Seconds
+//        echo "\n";
+//
+//        $datetime = new DateTime('2017-08-14 11:00:00');
+//        $datetime->sub(new TimeExpression('PT25M'));
+//        echo $datetime->format('Y-m-d H:i:s'); //2017-08-14 10:35:00');
+//
+//        $datetime = new DateTime('2017-08-14 11:00:00');
+//        $datetime->sub(new TimeExpression('25 min'));
+//        echo $datetime->format('Y-m-d H:i:s'); //2017-08-14 10:35:00');
+//
+//        $datetime = new DateTime('2017-08-14 11:00:00');
+//        $datetime->add(new TimeExpression(1501));
+//        echo $datetime->format('Y-m-d H:i:s'); //2017-08-14 10:35:00');
+
+//        $di = new DateInterval()
 
     }
 }
