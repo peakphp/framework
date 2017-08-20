0df3fed046b0d3ee108ed867a97421072d7106a8
Francois Lajoie <snake386@hotmail.com>
2017-08-20 09:32:40 -0400

    updated

--- tests/CommonTest/TimeExpressionTest.php
+++ tests/CommonTest/TimeExpressionTest.php
@@ -8,7 +8,6 @@ class TimeExpressionTest extends TestCase
 {
     function testExpression()
     {
-
         $this->assertTrue( (new TimeExpression(1))->toMicroseconds() === 1000);
         //$this->assertTrue( (new TimeExpression(1000))->toSeconds() === 1000);
         $this->assertTrue( (new TimeExpression("2day"))->toSeconds() === 172800);
@@ -27,23 +26,29 @@ class TimeExpressionTest extends TestCase
         $this->assertTrue( (new TimeExpression("4m 30s"))->toSeconds() === 270);
         $this->assertTrue( (new TimeExpression("4mins"))->toMicroseconds() === 240000);
         $this->assertTrue( (new TimeExpression("4mins"))->toSeconds() === 240);
-        echo (new TimeExpression("1 m"))->toSeconds();
-
-
-
         $this->assertTrue( (new TimeExpression(3705))->toString() === '1 hour 1 minute 45 seconds');
+
+    }
+
+    function testClockFormat()
+    {
         $this->assertTrue( (new TimeExpression('02:40:40'))->toSeconds() == 9640);
         $this->assertTrue( (new TimeExpression('4:30'))->toSeconds() == 270);
         $this->assertTrue( (new TimeExpression('4:30'))->toString() == '4 minutes 30 seconds');
+    }
 
+    function testEmpty()
+    {
         // 0 or empty
         $this->assertTrue( (new TimeExpression(0))->toSeconds() == 0);
         $this->assertTrue( (new TimeExpression(0))->toString() === '0 second');
         $this->assertTrue( (new TimeExpression(0))->toString('%h hour(s)') === '0 hour(s)');
         $this->assertTrue( (new TimeExpression(''))->toString('%h hour(s)') === '0 hour(s)');
         $this->assertTrue( (new TimeExpression(''))->toSeconds() == 0);
+    }
 
-
+    function testIntervalSpec()
+    {
         // using DateInterval syntax (ISO8601 interval spec)
         $this->assertTrue( (new TimeExpression("PT1H35M45S"))->toSeconds() === 5745);
         $this->assertTrue( (new TimeExpression("P2D"))->toSeconds() === 172800);
@@ -60,14 +65,6 @@ class TimeExpressionTest extends TestCase
             (new TimeExpression("P3DT5H"))->toString('%y years %d days %h hours') === '0 years 3 days 5 hours'
         );
 
-
-
-
-        // using TimeExpression to add time from a DateTime
-        $datetime = new DateTime('2017-08-14 11:00:00');
-        $datetime->add((new TimeExpression('25 mins 15 secs'))->toDateInterval());
-        $this->assertTrue($datetime->format('Y-m-d H:i:s') === '2017-08-14 11:25:15');
-
         // getting ISO8601 interval spec
         $this->assertTrue( (new TimeExpression('2 days 1 sec'))->toIntervalSpec() === 'P2DT1S');
 
@@ -76,6 +73,44 @@ class TimeExpressionTest extends TestCase
 
         // getting ISO8601 interval spec
         $this->assertTrue( (new TimeExpression("2 week 3 days"))->toIntervalSpec() === 'P17D');
+
+        $this->assertTrue(TimeExpression::dateIntervalToIntervalSpec(new DateInterval('P17D')) === 'P17D');
+    }
+
+    function testDiff()
+    {
+        // using TimeExpression to add time from a DateTime
+        $datetime = new DateTime('2017-08-14 11:00:00');
+        $datetime->add((new TimeExpression('25 mins 15 secs'))->toDateInterval());
+        $this->assertTrue($datetime->format('Y-m-d H:i:s') === '2017-08-14 11:25:15');
+    }
+
+    function testIsValid()
+    {
+        $this->assertTrue(TimeExpression::isValid('25 mins 15 secs'));
+        $this->assertTrue(TimeExpression::isValid('P2WT10H'));
+        $this->assertTrue(TimeExpression::isValid('4m 30s'));
+        $this->assertTrue(TimeExpression::isValid('12:20:15'));
+        $this->assertTrue(TimeExpression::isValid(234234));
+
+        $this->assertFalse(TimeExpression::isValid('asdas'));
+    }
+
+    function testCreateFrom()
+    {
+        $this->assertTrue(TimeExpression::createFrom('25 mins 15 secs') instanceof TimeExpression);
+        $this->assertFalse(TimeExpression::createFrom('asdas'));
+    }
+
+    function testException()
+    {
+        try {
+            (new TimeExpression("unknow"));
+        } catch (Exception $e) {
+            $error = true;
+        }
+
+        $this->assertTrue(isset($error));
     }
 }
 
