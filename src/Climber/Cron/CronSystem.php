4c76373637a6e47378b38ef540d468bd990bbb06
Francois Lajoie <snake386@hotmail.com>
2017-10-31 22:25:48 -0400

    renamed Cron to CronSystem and added static method connect()

--- src/Climber/Cron/CronSystem.php
+++ src/Climber/Cron/CronSystem.php
@@ -2,10 +2,12 @@
 
 namespace Peak\Climber\Cron;
 
+use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
 use \Exception;
 
-class Cron
+class CronSystem
 {
     /**
      * Check if ww have valid Doctrine connection instance
@@ -33,5 +35,15 @@ class Cron
         } catch (Exception $e) {
             return false;
         }
+    }
+
+    /**
+     * Connect
+     * @param array $database_config
+     * @return Connection
+     */
+    public static function connect(array $database_config)
+    {
+        return DriverManager::getConnection($database_config, new Configuration());
     }
 }
