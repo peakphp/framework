64663a969e4d340ca18482eeea6320a0c58569e7
Francois Lajoie <snake386@hotmail.com>
2017-12-06 00:49:15 -0500

    Updated to support custom log content closure

--- src/Common/ExceptionLogger.php
+++ src/Common/ExceptionLogger.php
@@ -7,6 +7,8 @@ class ExceptionLogger
 {
     const MODE_NORMAL = 1;
     const MODE_VERBOSE = 2;
+    const MODE_CUSTOM = 3;
+
     /**
      * Exception object
      * @var Exception
@@ -26,38 +28,81 @@ class ExceptionLogger
     protected $mode;
 
     /**
+     * @var mixed
+     */
+    protected $custom_closure;
+
+    /**
      * Constructor
      * @param Exception $exception
-     * @param $filepath
+     * @param string $filepath
      * @param int $mode
+     * @param null|callable $custom_closure
      */
-    public function __construct(Exception $exception, $filepath, $mode = self::MODE_NORMAL)
+    public function __construct(Exception $exception, $filepath, $mode = self::MODE_NORMAL, $custom_closure = null)
     {
         $this->exception = $exception;
         $this->filepath  = $filepath;
         $this->mode = $mode;
+        $this->custom_closure = $custom_closure;
         $this->log();
     }
 
     /**
-     * Log the error to a file
+     * Log exception
      */
     protected function log()
     {
         if ($this->mode == self::MODE_VERBOSE) {
-            $content = exceptionTrace($this->exception);
-            $content = strip_tags($content)."\n\n";
+            $content = $this->verboseContent();
+        } elseif ($this->mode == self::MODE_CUSTOM) {
+            if (!is_callable($this->custom_closure)) {
+                throw new Exception(__CLASS__.': you need to specify a closure when using custom mode');
+            }
+            $fn = $this->custom_closure;
+            $content = $fn($this->exception);
         } else {
-            $msg = trim($this->exception->getMessage());
-            $content = '['.date('Y-m-d H:i:s')."] ".get_class($this->exception)."\n";
-            $content .= $msg."\n\n";
+            $content = $this->normalContent();
         }
 
+        $this->write($content);
+    }
+
+    /**
+     * Write content
+     * @param $content
+     * @throws Exception
+     */
+    protected function write($content)
+    {
         if ( (!file_exists($this->filepath) && !is_writable(dirname($this->filepath))) ||
             (file_exists($this->filepath) && !is_writable($this->filepath))) {
             throw new Exception(__CLASS__.': cannot write to ['.$this->filepath.']');
         }
 
         file_put_contents($this->filepath, $content, FILE_APPEND);
+    }
+
+    /**
+     * MODE_VERBOSE
+     * @return string
+     */
+    protected function verboseContent()
+    {
+        $content = exceptionTrace($this->exception);
+        $content = strip_tags($content)."\n\n";
+        return $content;
+    }
+
+    /**
+     * MODE_NORMAL
+     * @return string
+     */
+    protected function normalContent()
+    {
+        $msg = trim($this->exception->getMessage());
+        $content = '['.date('Y-m-d H:i:s')."] ".get_class($this->exception)."\n";
+        $content .= $msg."\n\n";
+        return $content;
     }
 }
