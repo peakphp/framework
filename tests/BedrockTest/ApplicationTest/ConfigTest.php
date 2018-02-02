1c66699f7d260d38a38855ac4e767fcda7d53d3b
Francois Lajoie <snake386@hotmail.com>
2018-02-01 22:39:39 -0500

    added ConfigTest

---
+++ tests/BedrockTest/ApplicationTest/ConfigTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use Peak\Bedrock\Application\Config;
+
+class ConfigTest extends TestCase
+{
+    function testCreate()
+    {
+        $config = new Config();
+    }
+
+}
