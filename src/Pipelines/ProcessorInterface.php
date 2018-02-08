5546866c8b55d1ffe773685e31f804ec9e02b88e
Francois Lajoie <snake386@hotmail.com>
2018-02-08 15:08:43 -0500

    added comments

--- src/Pipelines/ProcessorInterface.php
+++ src/Pipelines/ProcessorInterface.php
@@ -2,7 +2,17 @@
 
 namespace Peak\Pipelines;
 
+/**
+ * Interface ProcessorInterface
+ */
 interface ProcessorInterface
 {
+    /**
+     * Process pipes
+     *
+     * @param array $pipes
+     * @param mixed $payload
+     * @return mixed
+     */
     public function process(array $pipes, $payload);
 }
