9269df42276e7ace367df4af88f357e547fc9c3c
Francois Lajoie <snake386@hotmail.com>
2018-02-08 15:09:17 -0500

    updated to use InvalidPipeException and MissingPipeInterfaceException

--- src/Pipelines/AbstractProcessor.php
+++ src/Pipelines/AbstractProcessor.php
@@ -2,9 +2,10 @@
 
 namespace Peak\Pipelines;
 
+use Peak\Pipelines\Exceptions\InvalidPipeException;
+use Peak\Pipelines\Exceptions\MissingPipeInterfaceException;
 use Psr\Container\ContainerInterface;
 use \Closure;
-use \Exception;
 
 abstract class AbstractProcessor
 {
@@ -26,42 +27,67 @@ abstract class AbstractProcessor
     /**
      * Resolve a pipe execution
      *
-     * @param  mixed $pipe     Support closure, class instance, class string or pipeline instance
+     * @param  mixed $pipe     Support closure, class instance, class string, pipeline instance and pipe function name
      * @param  mixed $payload  Payload pass to each pipe
      * @return mixed
+     * @throws MissingPipeInterfaceException
+     * @throws InvalidPipeException
      */
     protected function resolvePipe($pipe, $payload)
     {
         if ($pipe instanceof Closure) {
-            // for closure
+            // process pipe closure
             return call_user_func($pipe, $payload);
-        } elseif (is_callable($pipe)) {
-            return $this->processPipeInstance($pipe, $payload);
-        } elseif (is_string($pipe) && class_exists($pipe)) {
-            // class string with or without di
-            $pinst = (isset($this->container)) ? $this->container->create($pipe) : new $pipe();
-            if (!$pinst instanceof PipeInterface) {
-                throw new Exception('Pipe "'.$pipe.'" must implements PipeInterface');
-            }
-            return $this->processPipeInstance($pinst, $payload);
         } elseif ($pipe instanceof Pipeline) {
-            // another pipeline instance
+            // process another pipeline instance
             return $pipe->process($payload);
+        } elseif (is_string($pipe) && class_exists($pipe)) {
+            // process pipe class name
+            return $this->processPipeClassName($pipe, $payload);
+        } elseif(is_object($pipe)) {
+            // process pipe object instance
+            return $this->processPipeObject($pipe, $payload);
+        } elseif (is_callable($pipe)) {
+            // process callable pipe function
+            return $pipe($payload);
         }
 
         // unknown pipe type
-        throw new Exception('Invalid pipe type');
+        throw new InvalidPipeException();
     }
 
     /**
-     * Process a pipe class
+     * Process Pipe string class name
      *
-     * @param  PipeInterface $pipe
-     * @param  mixed         $payload
+     * @param string $pipe
+     * @param mixed $payload
      * @return mixed
+     * @throws MissingPipeInterfaceException
      */
-    protected function processPipeInstance(PipeInterface $pipe, $payload)
+    protected function processPipeClassName($pipe, $payload)
     {
+        $pinst = (isset($this->container)) ? $this->container->create($pipe) : new $pipe();
+
+        if (!$pinst instanceof PipeInterface) {
+            throw new MissingPipeInterfaceException(get_class($pinst));
+        }
+        return $pinst($payload);
+    }
+
+    /**
+     * Process pipe class object
+     *
+     * @param object $pipe
+     * @param mixed $payload
+     * @return mixed
+     * @throws MissingPipeInterfaceException
+     */
+    protected function processPipeObject($pipe, $payload)
+    {
+        if (!$pipe instanceof PipeInterface) {
+            throw new MissingPipeInterfaceException(get_class($pipe));
+        }
         return $pipe($payload);
     }
+
 }
