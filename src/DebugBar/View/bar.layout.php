fcf69675313fc902acb8feebe7418ca8ffe16699
Francois Lajoie <snake386@hotmail.com>
2018-02-10 12:35:38 -0500

    renamed layout file

--- src/DebugBar/View/bar.layout.php
+++ src/DebugBar/View/bar.layout.php
@@ -2,7 +2,7 @@
     <div class="pkdebugbar-menu-wrapper">
         <ul class="pkdebugbar-menu">
             <?php
-            foreach ($tabs as $tab_name => $tab_content) {
+            foreach ($view->tabs as $tab_name => $tab_content) {
                 echo '<li class="pkdebugbar-tab" data-target="'.$tab_name.'">'.$tab_content.'</li>';
             }
             ?>
