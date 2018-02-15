a4de5d4918312cb35753c5e29fd498e831fcf1de
Francois Lajoie <snake386@hotmail.com>
2018-02-15 01:34:30 -0500

    added

---
+++ src/DebugBar/Modules/Message/view.php
@@ -0,0 +1,9 @@
+<h1>Message(s)</h1>
+<?php
+foreach ($view->messages as $message) {
+    echo '<div class="message message-'.$message->level.'">
+            '.$message->content.'
+            <div class="message-level">'.$message->level.'</div>
+          </div>';
+}
+?>
