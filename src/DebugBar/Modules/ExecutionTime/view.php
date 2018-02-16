666231d6e702f3ad0dbc2e0eaa51a792c5b93be6
Francois Lajoie <snake386@hotmail.com>
2018-02-16 10:42:45 -0500

    updated css classes

--- src/DebugBar/Modules/ExecutionTime/view.php
+++ src/DebugBar/Modules/ExecutionTime/view.php
@@ -1,29 +1,39 @@
 <h1>Execution time</h1>
 
-<h3><div class="hblock">Current Request</div></h3>
-<?php echo $view->stats['current_request']['uri']; ?><br>
-<?php echo $view->stats['current_request']['time']; ?> sec.
+<h3><div class="h-block">Current Request</div></h3>
+<span class="strong-block"><?php echo $view->stats['current_request']['time']; ?></span>
+<span class="strong-block"><?php echo $view->stats['current_request']['uri']; ?></span>
 
 <?php if (!empty($view->stats['last_request'])) : ?>
-    <h3><div class="hblock">Last request</div></h3>
-    <?php echo $view->stats['last_request']['uri']; ?><br>
-    <?php echo $view->stats['last_request']['time']; ?> sec.
+    <h3><div class="h-block">Last request</div></h3>
+    <span class="strong-block"><?php echo $view->stats['last_request']['time']; ?></span>
+    <span class="strong-block"><?php echo $view->stats['last_request']['uri']; ?></span>
 <?php endif; ?>
 
-<h3><div class="hblock">Average request time</div></h3>
-<?php echo $view->stats['average_request']; ?> sec.
+<h3><div class="h-block">Average request time</div></h3>
+<span class="strong-block"><?php echo $view->stats['average_request']; ?></span>
 
 <?php if (!empty($view->stats['shortest_request'])) : ?>
-    <h3><div class="hblock">Shortest request time</div></h3>
-    <?php echo $view->stats['shortest_request']['uri']; ?><br>
-    <?php echo $view->stats['shortest_request']['time']; ?> sec.
+    <h3><div class="h-block">Shortest request time</div></h3>
+    <span class="strong-block"><?php echo $view->stats['shortest_request']['time']; ?></span>
+    <span class="strong-block"><?php echo $view->stats['shortest_request']['uri']; ?></span>
 <?php endif; ?>
 
 <?php if (!empty($view->stats['longest_request'])) : ?>
-    <h3><div class="hblock">Longest request time</div></h3>
-    <?php echo $view->stats['longest_request']['uri']; ?> -
-    <code><?php echo $view->stats['longest_request']['time']; ?> sec.</code>
+    <h3><div class="h-block">Longest request time</div></h3>
+    <span class="strong-block"><?php echo $view->stats['longest_request']['time']; ?></span>
+    <span class="strong-block"><?php echo $view->stats['longest_request']['uri']; ?></span>
 <?php endif; ?>
 
-<pre>
-<?php //print_r($view); ?>
+<h3><div class="h-block">Stats</div></h3>
+<table style="width:auto;">
+<?php
+foreach ($view->stats['requests_avg'] as $uri => $stats) {
+    echo '<tr>
+            <td class="width-1"><strong>'.$uri.'</strong></td>
+            <td class="width-1">'.$stats['average'].'</td>
+            <td class="width-1">'.$stats['count'].'</td>
+          </tr>';
+}
+?>
+</table>
--- src/DebugBar/Modules/Inputs/view.php
+++ src/DebugBar/Modules/Inputs/view.php
@@ -1,13 +1,13 @@
 <?php use Peak\DebugBar\View\Helper\ArrayTable; ?>
 <h1>Inputs information</h1>
-<h3><div class="hblock">GET</div></h3>
+<h3><div class="h-block">GET</div></h3>
 <?php echo (new ArrayTable($view->get))->render(); ?>
-<h3><div class="hblock">POST</div></h3>
+<h3><div class="h-block">POST</div></h3>
 <?php echo (new ArrayTable($view->post))->render(); ?>
-<h3><div class="hblock">COOKIE</div></h3>
+<h3><div class="h-block">COOKIE</div></h3>
 <?php echo (new ArrayTable($view->cookie))->render(); ?>
-<h3><div class="hblock">SERVER</div></h3>
+<h3><div class="h-block">SERVER</div></h3>
 <?php echo (new ArrayTable($view->server))->render(); ?>
-<h3><div class="hblock">ENV</div></h3>
+<h3><div class="h-block">ENV</div></h3>
 <?php echo (new ArrayTable($view->env))->render(); ?>
 
