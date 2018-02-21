0ac805ba9b8b16cb6a0fa97045da2973f8181c1a
Francois Lajoie <snake386@hotmail.com>
2018-02-21 00:31:04 -0500

    updated

--- tests/RoutingTest/RouteBuilderTest.php
+++ tests/RoutingTest/RouteBuilderTest.php
@@ -18,14 +18,12 @@ class RouteBuilderTest extends TestCase
         $this->assertTrue($route->controller === 'ctrl');
         $this->assertTrue($route->action === 'action');
         $this->assertTrue(empty($route->params));
-        $this->assertTrue(empty($route->params_assoc));
 
 
         $route = RouteBuilder::get('ctrl', 'action', '');
         $this->assertTrue($route->controller === 'ctrl');
         $this->assertTrue($route->action === 'action');
         $this->assertTrue(empty($route->params));
-        $this->assertTrue(empty($route->params_assoc));
 
     }
 
@@ -35,41 +33,31 @@ class RouteBuilderTest extends TestCase
         $route = RouteBuilder::get('ctrl', 'action', 'test', 'test2');
 
         $this->assertFalse(empty($route->params));
-        $this->assertFalse(empty($route->params_assoc));
         $this->assertTrue(count($route->params) == 2);
-        $this->assertTrue($route->params_assoc['test'] === 'test2');
 
 
         $route = RouteBuilder::get('ctrl', 'action', ['test', 'test2']);
 
         $this->assertFalse(empty($route->params));
-        $this->assertFalse(empty($route->params_assoc));
         $this->assertTrue(count($route->params) == 4);
-        $this->assertTrue($route->params_assoc[0] === 'test');
 
 
         $route = RouteBuilder::get('ctrl', 'action', ['test' => 'test2']);
 
         $this->assertFalse(empty($route->params));
-        $this->assertFalse(empty($route->params_assoc));
         $this->assertTrue(count($route->params) == 2);
-        $this->assertTrue($route->params_assoc['test'] === 'test2');
 
 
         $route = RouteBuilder::get('ctrl', 'action', ['test' => 'test2'], ['test' => 'test2']);
 
         $this->assertFalse(empty($route->params));
-        $this->assertFalse(empty($route->params_assoc));
         $this->assertTrue(count($route->params) == 4);
-        $this->assertTrue(count($route->params_assoc) == 1);
 
 
         $route = RouteBuilder::get('ctrl', 'action', 'test','test2','this','funky','extra');
 
         $this->assertFalse(empty($route->params));
-        $this->assertFalse(empty($route->params_assoc));
         $this->assertTrue(count($route->params) == 5);
-        $this->assertTrue(count($route->params_assoc) == 2);
     }
 
 
