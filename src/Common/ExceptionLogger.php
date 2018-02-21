1961c3a4b7c7982e0e984da7a63d59e817e298f5
Francois Lajoie <snake386@hotmail.com>
2018-02-20 23:57:47 -0500

    added a new mode and refactored log()

--- src/Common/ExceptionLogger.php
+++ src/Common/ExceptionLogger.php
@@ -7,7 +7,15 @@ class ExceptionLogger
 {
     const MODE_NORMAL = 1;
     const MODE_VERBOSE = 2;
-    const MODE_CUSTOM = 3;
+    const MODE_CLOSURE = 3;
+    const MODE_CUSTOM = 4;
+
+    protected $modes = [
+        self::MODE_NORMAL => 'normalContent',
+        self::MODE_VERBOSE => 'verboseContent',
+        self::MODE_CLOSURE => 'closureContent',
+        self::MODE_CUSTOM => 'customContent',
+    ];
 
     /**
      * Exception object
@@ -33,37 +41,57 @@ class ExceptionLogger
     protected $custom_closure;
 
     /**
+     * @var array
+     */
+    protected $default_content = [];
+
+    /**
      * Constructor
      * @param Exception $exception
      * @param string $filepath
      * @param int $mode
-     * @param null|callable $custom_closure
+     * @param mixed $custom
+     * @throws Exception
      */
-    public function __construct(Exception $exception, $filepath, $mode = self::MODE_NORMAL, $custom_closure = null)
+    public function __construct(Exception $exception, $filepath, $mode = self::MODE_NORMAL, $custom = null)
     {
         $this->exception = $exception;
         $this->filepath  = $filepath;
         $this->mode = $mode;
-        $this->custom_closure = $custom_closure;
+        $this->custom = $custom;
+
+        $this->initDefaultContent();
         $this->log();
     }
 
     /**
+     * Default content
+     */
+    protected function initDefaultContent()
+    {
+        $this->default_content = [
+            'datetime' => date('Y-m-d H:i:s'),
+            'date' => date('Y-m-d'),
+            'time' => date('H:i:s'),
+            'exception' => trim($this->exception->getMessage()),
+            'exception_class' => get_class($this->exception),
+            'exception_name' => shortClassName($this->exception),
+        ];
+    }
+
+    /**
      * Log exception
+     *
+     * @throws Exception
      */
     protected function log()
     {
-        if ($this->mode == self::MODE_VERBOSE) {
-            $content = $this->verboseContent();
-        } elseif ($this->mode == self::MODE_CUSTOM) {
-            if (!is_callable($this->custom_closure)) {
-                throw new Exception(__CLASS__.': you need to specify a closure when using custom mode');
-            }
-            $fn = $this->custom_closure;
-            $content = $fn($this->exception);
-        } else {
-            $content = $this->normalContent();
+        if (!isset($this->modes[$this->mode])) {
+            throw new Exception(__CLASS__.' mode is invalid');
         }
+
+        $method = $this->modes[$this->mode];
+        $content = $this->$method();
 
         $this->write($content);
     }
@@ -95,14 +123,37 @@ class ExceptionLogger
     }
 
     /**
+     * MODE_CLOSURE
+     * @return string
+     * @throws Exception
+     */
+    protected function closureContent()
+    {
+        if (!is_callable($this->custom_closure)) {
+            throw new Exception(__CLASS__.': you need to specify a closure when using custom mode');
+        }
+        $fn = $this->custom_closure;
+        return $fn($this->exception);
+    }
+
+    /**
+     * MODE_CUSTOM
+     * @return string
+     * @throws Exception
+     */
+    protected function customContent()
+    {
+        $content = array_merge($this->default_content, $this->custom['content']);
+        return interpolate($this->custom['message'], $content);
+    }
+
+    /**
      * MODE_NORMAL
      * @return string
      */
     protected function normalContent()
     {
-        $msg = trim($this->exception->getMessage());
-        $content = '['.date('Y-m-d H:i:s')."] ".get_class($this->exception)."\n";
-        $content .= $msg."\n\n";
-        return $content;
+        $message = "[{datetime}] {exception_class}\n{exception}\n\n";
+        return interpolate($message, $this->default_content);
     }
 }
