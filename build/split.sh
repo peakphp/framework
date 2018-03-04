9da2aa11ddc989b03f806eaa3a0738539de1aba1
Francois Lajoie <snake386@hotmail.com>
2018-03-04 12:06:44 -0500

    added pipelines

--- build/split.sh
+++ build/split.sh
@@ -3,4 +3,5 @@ git subsplit publish --heads="master" src/Common:git@github.com:peakphp/common.g
 git subsplit publish --heads="master" src/Config:git@github.com:peakphp/config.git
 git subsplit publish --heads="master" src/DebugBar:git@github.com:peakphp/debugbar.git
 git subsplit publish --heads="master" src/Di:git@github.com:peakphp/di.git
+git subsplit publish --heads="master" src/Pipelines:git@github.com:peakphp/pipelines.git
 rm -rf .subsplit/
