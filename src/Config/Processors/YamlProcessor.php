db322419c6ecc80e96bd4a3573dabfca92ee9719
Francois Lajoie <snake386@hotmail.com>
2018-03-03 21:34:37 -0500

    update comment

--- src/Config/Processors/YamlProcessor.php
+++ src/Config/Processors/YamlProcessor.php
@@ -4,13 +4,14 @@ namespace Peak\Config\Processors;
 
 use Peak\Config\AbstractProcessor;
 use Peak\Config\Exceptions\ProcessorException;
+use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
 
 class YamlProcessor extends AbstractProcessor
 {
     /**
      * @param $data
-     * @throws ProcessorException
+     * @throws ProcessorException, ParseException
      */
     public function process($data)
     {
