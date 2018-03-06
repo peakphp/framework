c2f3a0eb23d3c3a39e1fb39f29392ab0df4f5b6a
Francois Lajoie <snake386@hotmail.com>
2018-03-06 00:07:01 -0500

    added exceptions

---
+++ src/Config/Exceptions/CachePathNotWritableException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Peak\Config\Exceptions;
+
+class CachePathNotWritableException extends \Exception
+{
+    /**
+     * CachePathNotWritableException constructor.
+     *
+     * @param string $path
+     */
+    public function __construct(string $path)
+    {
+        parent::__construct('Config cache path "'.$path.'"" not writable');
+    }
+}
