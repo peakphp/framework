cb9dbee91ad488c514302e7fe2743f6e98c2e769
Francois Lajoie <snake386@hotmail.com>
2018-05-20 10:23:42 -0400

    updated build script

--- build/split-notags.sh
+++ build/split-notags.sh
@@ -1,6 +1,5 @@
 git subsplit init git@github.com:peakphp/framework.git
 git subsplit publish --no-tags --heads="master" src/Common:git@github.com:peakphp/common.git
-git subsplit publish --no-tags --heads="master" src/Config:git@github.com:peakphp/config.git
 git subsplit publish --no-tags --heads="master" src/DebugBar:git@github.com:peakphp/debugbar.git
 git subsplit publish --no-tags --heads="master" src/Di:git@github.com:peakphp/di.git
 git subsplit publish --no-tags --heads="master" src/Pipelines:git@github.com:peakphp/pipelines.git
