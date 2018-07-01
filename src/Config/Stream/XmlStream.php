df701ddbc33db4967ce460e8cf01ffe1cd4f603f
Francois Lajoie <snake386@hotmail.com>
2018-07-01 14:34:31 -0400

    added XmlStream

---
+++ src/Config/Stream/XmlStream.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Peak\Config\Stream;
+
+use Peak\Config\Processor\XmlProcessor;
+
+class XmlStream extends DataStream
+{
+    /**
+     * XmlStream constructor.
+     * @param $xml
+     */
+    public function __construct($xml)
+    {
+        parent::__construct($xml, new XmlProcessor());
+    }
+}
