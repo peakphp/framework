0b009d8ad92af6043376fc212289d158a64c6337
Francois Lajoie <snake386@hotmail.com>
2018-07-08 09:52:59 -0400

    updated console config to reflect changes with the config component

--- src/Climber/ConfigResolver.php
+++ src/Climber/ConfigResolver.php
@@ -3,7 +3,7 @@
 namespace Peak\Climber;
 
 use Peak\Bedrock\Application\Config;
-use Peak\Config\ConfigLoader;
+use Peak\Config\ConfigFactory;
 
 class ConfigResolver
 {
@@ -25,15 +25,15 @@ class ConfigResolver
     ];
 
     /**
-     * The final config collection
-     * @var object
+     * @var Config
      */
     protected $config;
 
     /**
-     * Construct
+     * ConfigResolver constructor.
      *
-     * @param array $config user config
+     * @param array $config
+     * @throws \Peak\Config\Exception\UnknownResourceException
      */
     public function __construct(array $config = [])
     {
@@ -52,9 +52,8 @@ class ConfigResolver
             }
         }
 
-        $this->config = new Config(
-            (new ConfigLoader($final))->asArray()
-        );
+        $configFactory = new ConfigFactory();
+        $this->config = $configFactory->loadResourcesWith($final, new Config());
 
         if (!defined('APPLICATION_ENV')) {
             define('APPLICATION_ENV', $this->config->env);
@@ -62,8 +61,6 @@ class ConfigResolver
     }
 
     /**
-     * Get app configuration
-     *
      * @return Config
      */
     public function getMountedConfig()
