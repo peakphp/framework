7e3379e5bec3455dec344e9e82112a8dd6bb3152
Francois Lajoie <snake386@hotmail.com>
2018-07-08 11:31:49 -0400

    added

---
+++ src/Bedrock/Application/Exceptions/PathNotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Peak\Bedrock\Application\Exceptions;
+
+class PathNotFoundException extends \Exception
+{
+    /**
+     * PathNotFoundException constructor.
+     * @param $path
+     * @param null $context
+     */
+    public function __construct($path, $context = null)
+    {
+        $str = 'Path '.trim(strip_tags($path)).' not found!';
+        if (isset($context)) {
+            $str .= ' Context: '.$context;
+        }
+        parent::__construct($str);
+    }
+}
