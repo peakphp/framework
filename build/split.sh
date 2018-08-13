41baf72ad31186cacb65ef4d51db6ca312f1df82
Francois Lajoie <snake386@hotmail.com>
2018-08-12 20:49:38 -0400

    remove collection for now, since it depend on common component

--- build/split.sh
+++ build/split.sh
@@ -1,6 +1,5 @@
 git subsplit init git@github.com:peakphp/framework.git
 git subsplit publish --heads="master" src/Common:git@github.com:peakphp/common.git
-git subsplit publish --heads="master" src/Common/Collection:git@github.com:peakphp/collection.git
 git subsplit publish --heads="master" src/Config:git@github.com:peakphp/config.git
 git subsplit publish --heads="master" src/DebugBar:git@github.com:peakphp/debugbar.git
 git subsplit publish --heads="master" src/Di:git@github.com:peakphp/di.git
