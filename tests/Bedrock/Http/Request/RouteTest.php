ab187bed8010b76f587105cbbebd416681e1611a
Francois Lajoie <snake386@hotmail.com>
2018-08-13 09:28:24 -0400

    updated

--- tests/Bedrock/Http/Request/RouteTest.php
+++ tests/Bedrock/Http/Request/RouteTest.php
@@ -35,8 +35,65 @@ class RouteTest extends TestCase
 
         $result = $route->match($request);
         $this->assertFalse($result);
+    }
+
+    public function testMatch2()
+    {
+        $route = new Route(
+            'GET',
+            '/mypath',
+            $this->createMock(StackInterface::class)
+        );
+
+        $request = $this->createMock(ServerRequestInterface::class);
 
         $request->expects($this->once())
+            ->method('getMethod')
+            ->will($this->returnValue('GET'));
+
+        $uri = $this->createMock(UriInterface::class);
+        $uri->expects(($this->once()))
+            ->method('getPath')
+            ->will($this->returnValue('/mypath'));
+
+        $request->expects($this->once())
+            ->method('getUri')
+            ->will($this->returnValue($uri));
+
+        $result = $route->match($request);
+        $this->assertTrue($result);
+    }
+
+
+    public function testMatch3()
+    {
+        $route = new Route(
+            'POST',
+            '/mypath',
+            $this->createMock(StackInterface::class)
+        );
+
+        $request = $this->createMock(ServerRequestInterface::class);
+
+        $request->expects($this->once())
+            ->method('getMethod')
+            ->will($this->returnValue('GET'));
+
+        $result = $route->match($request);
+        $this->assertFalse($result);
+    }
+
+    public function testMatch4()
+    {
+        $route = new Route(
+            null,
+            '/mypath',
+            $this->createMock(StackInterface::class)
+        );
+
+        $request = $this->createMock(ServerRequestInterface::class);
+
+        $request->expects($this->never())
             ->method('getMethod')
             ->will($this->returnValue('GET'));
 
