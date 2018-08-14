b1071400c5ceb400e9285b9dbbf177a239528788
Francois Lajoie <snake386@hotmail.com>
2018-08-14 10:15:18 -0400

    updated

--- tests/ConfigTest/Cache/FileCacheTest.php
+++ tests/ConfigTest/Cache/FileCacheTest.php
@@ -27,4 +27,40 @@ class FileCacheTest extends TestCase
         $this->assertFalse(file_exists($cacheFile));
     }
 
+    public function testDeleteMultiple()
+    {
+        $cachePath = __DIR__.'/tmp';
+
+        $id1 = 'cache1';
+        $id2 = 'cache2';
+
+        $fileCache = new FileCache($cachePath);
+        $this->assertFalse($fileCache->deleteMultiple([$id1, $id2]));
+
+        $this->assertTrue($fileCache->set($id1, 'foobar'));
+        $this->assertFalse($fileCache->deleteMultiple([$id1, $id2]));
+
+        $this->assertTrue($fileCache->setMultiple([$id1 => 'foobar', $id2 => 'foobar']));
+        $this->assertTrue($fileCache->deleteMultiple([$id1, $id2]));
+    }
+
+    public function testSetMultiple()
+    {
+        $cachePath = __DIR__.'/tmp';
+
+        $id1 = 'cache1';
+        $id2 = 'cache2';
+
+        $fileCache = new FileCache($cachePath);
+        $this->assertTrue($fileCache->setMultiple([
+            $id1 => 'test1',
+            $id2 => 'test2'
+        ], -100));
+        $this->assertTrue($fileCache->has($id1));
+        $this->assertTrue($fileCache->has($id2));
+        $this->assertTrue($fileCache->isExpired($id1));
+        $this->assertTrue($fileCache->isExpired($id2));
+        $this->assertTrue($fileCache->clear());
+    }
+
 }
