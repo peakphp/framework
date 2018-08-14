60fff2dd56b78dae95ae1cd8b19aa3ff02387809
Francois Lajoie <snake386@hotmail.com>
2018-08-14 17:00:47 -0400

    updated

--- tests/CommonTest/CollectionTest/PropertiesBagTest.php
+++ tests/CommonTest/CollectionTest/PropertiesBagTest.php
@@ -10,12 +10,22 @@ class PropertiesBagTest extends TestCase
     {
         $propertiesBag = new PropertiesBag(['foo' => 'bar']);
         $this->assertTrue(isset($propertiesBag->foo));
+        $this->assertTrue(isset($propertiesBag['foo']));
         $this->assertTrue($propertiesBag->foo === 'bar');
         $this->assertTrue(count($propertiesBag) == 1);
         $this->assertTrue(count($propertiesBag->toArray()) == 1);
+        $propertiesBag->foo = 'bar2';
+        $this->assertTrue($propertiesBag->foo === 'bar2');
         unset($propertiesBag->foo);
+
         $this->assertFalse(isset($propertiesBag->foo));
         $this->assertTrue(count($propertiesBag) == 0);
+        $propertiesBag['foo'] = 'bar';
+        $propertiesBag['bar'] = 'foo';
+        $this->assertTrue($propertiesBag['foo'] === 'bar');
+        $this->assertTrue($propertiesBag['bar'] === 'foo');
+        unset($propertiesBag['foo']);
+        $this->assertFalse(isset($propertiesBag['foo']));
     }
 
     /**
