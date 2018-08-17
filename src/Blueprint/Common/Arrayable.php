5a009926b846ba5d491d279612299217b5f86cce
Francois Lajoie <snake386@hotmail.com>
2018-08-17 17:06:09 -0400

    added

---
+++ src/Blueprint/Common/Arrayable.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Peak\Blueprint\Common;
+
+interface Arrayable
+{
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray();
+}
+
