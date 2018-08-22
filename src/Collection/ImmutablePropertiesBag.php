93d9a345e234f9fef9ac75c2c4932d676bcee6bf
Francois Lajoie <snake386@hotmail.com>
2018-08-22 08:32:36 -0400

    update exception name

--- src/Collection/ImmutablePropertiesBag.php
+++ src/Collection/ImmutablePropertiesBag.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Peak\Collection;
 
-use \Exception;
+use Peak\Collection\Exception\ImmutableException;
 
 /**
  * Class ImmutablePropertiesBag
@@ -14,37 +14,37 @@ class ImmutablePropertiesBag extends PropertiesBag
 {
     /**
      * Disable overriding of __set and throw exception if used
-     * @throws Exception
+     * @throws ImmutableException
      */
-    final public function __set($property, $val)
+    final public function __set(string $property, $val)
     {
-        throw new Exception(__CLASS__.': properties of immutable object cannot be modified');
+        throw new ImmutableException('modified');
     }
 
     /**
      * Disable overriding of __unset and throw exception if used
-     * @throws Exception
+     * @throws ImmutableException
      */
-    final public function __unset($property)
+    final public function __unset(string $property)
     {
-        throw new Exception(__CLASS__.': properties of immutable object cannot be unset');
+        throw new ImmutableException('unset');
     }
 
     /**
      * Assign a value to the specified offset
-     * @throws Exception
+     * @throws ImmutableException
      */
     final public function offsetSet($offset, $value)
     {
-        throw new Exception(__CLASS__.': properties of immutable object cannot be modified');
+        throw new ImmutableException('modified');
     }
 
     /**
      * Item to delete
-     * @throws Exception
+     * @throws ImmutableException
      */
     final public function offsetUnset($offset)
     {
-        throw new Exception(__CLASS__.': properties of immutable object cannot be unset');
+        throw new ImmutableException('unset');
     }
 }
