c7a0c9585fee38e6e5268bbca7a4510ed4646000
Francois Lajoie <snake386@hotmail.com>
2018-08-22 12:49:07 -0400

    updated

--- build/split.sh
+++ build/split.sh
@@ -1,4 +1,5 @@
 git subsplit init git@github.com:peakphp/framework.git
+git subsplit publish --heads="master" src/Blueprint:git@github.com:peakphp/blueprint.git
 git subsplit publish --heads="master" src/Collection:git@github.com:peakphp/collection.git
 git subsplit publish --heads="master" src/Common:git@github.com:peakphp/common.git
 git subsplit publish --heads="master" src/Config:git@github.com:peakphp/config.git
