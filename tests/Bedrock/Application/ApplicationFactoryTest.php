b7949c04e409941567d770361f8b66d9f1f5971a
Francois Lajoie <snake386@hotmail.com>
2018-08-23 12:40:57 -0400

    updated

--- tests/Bedrock/Application/ApplicationFactoryTest.php
+++ tests/Bedrock/Application/ApplicationFactoryTest.php
@@ -3,6 +3,7 @@
 use \PHPUnit\Framework\TestCase;
 use \Peak\Bedrock\Application\Application;
 use \Peak\Bedrock\Application\ApplicationFactory;
+use \Peak\Blueprint\Bedrock\Kernel;
 use \Peak\Blueprint\Common\ResourceResolver;
 use \Psr\Container\ContainerInterface;
 
@@ -18,6 +19,20 @@ class ApplicationFactoryTest extends TestCase
         $app = $appFactory->create(
             'dev',
             $this->createMock(ContainerInterface::class),
+            $this->createMock(ResourceResolver::class),
+            '2.0'
+        );
+
+        $this->assertInstanceOf(Application::class, $app);
+        $this->assertTrue($app->getVersion() === '2.0');
+    }
+
+    public function testCreateFromKernel()
+    {
+        $appFactory = new ApplicationFactory();
+
+        $app = $appFactory->createFromKernel(
+            $this->createMock(Kernel::class),
             $this->createMock(ResourceResolver::class),
             '2.0'
         );
