6cff92d4bdc83268c7db7c446bd8942836a9b682
Francois Lajoie <snake386@hotmail.com>
2018-08-23 12:41:14 -0400

    updated namespace

--- src/Bedrock/Application/Config.php
+++ src/Bedrock/Application/Config.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 namespace Peak\Bedrock\Application;
 
-use Peak\Config\Config as BaseConfig;
 
 /**
  * Class Config
  * @package Peak\Bedrock\Application
  */
-class Config extends BaseConfig
+class Config extends \Peak\Config\Config
 {
 }
