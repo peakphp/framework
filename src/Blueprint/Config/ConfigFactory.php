8696629855a7ffa43f098fd71a5e4f815064c298
Franck <snake386@hotmail.com>
2018-09-08 17:03:22 -0400

    added

---
+++ src/Blueprint/Config/ConfigFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Peak\Blueprint\Config;
+
+/**
+ * Interface ConfigFactory
+ * @package Peak\Blueprint\Config
+ */
+interface ConfigFactory
+{
+    /**
+     * Load config with default Peak\Config
+     *
+     * @param array $resources
+     * @return Config
+     */
+    public function loadResources(array $resources): Config;
+
+    /**
+     * Load config with custom Peak\Blueprint\Config\Config
+     *
+     * @param array $resources
+     * @param Config $config
+     * @return Config
+     */
+    public function loadResourcesWith(array $resources, Config $config): Config;
+}
