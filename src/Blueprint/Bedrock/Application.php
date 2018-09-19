2bcbdda0465f75d4b17c8d5206a4eadd26606b65
Franck <snake386@hotmail.com>
2018-09-18 22:05:04 -0400

    moved under Bedrock and added missing basic methods

---
+++ src/Blueprint/Bedrock/Application.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Peak\Blueprint\Bedrock;
+
+use Peak\Blueprint\Collection\Dictionary;
+use Peak\Blueprint\Http\ResponseEmitter;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Interface Application
+ * @package Peak\Blueprint\Bedrock
+ */
+interface Application extends RequestHandlerInterface
+{
+    /**
+     * @return Kernel
+     */
+    public function getKernel(): Kernel;
+
+    /**
+     * @param string $property
+     * @param null $default
+     * @return mixed
+     */
+    public function getProp(string $property, $default = null);
+
+    /**
+     * @param string $property
+     * @return bool
+     */
+    public function hasProp(string $property): bool;
+
+    /**
+     * Get application "properties" object
+     * @return Dictionary
+     */
+    public function getProps(): Dictionary;
+
+    /**
+     * Handle the request and emit a response
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseEmitter $emitter
+     * @return mixed
+     */
+    public function run(ServerRequestInterface $request, ResponseEmitter $emitter);
+}
