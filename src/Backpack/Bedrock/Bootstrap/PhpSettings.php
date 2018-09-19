e732d4af84f9581bed74cb25a2dd07c39c90418c
Franck <snake386@hotmail.com>
2018-09-18 23:13:18 -0400

    added

---
+++ src/Backpack/Bedrock/Bootstrap/PhpSettings.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Peak\Backpack\Bedrock\Bootstrap;
+
+use Peak\Blueprint\Common\Bootable;
+use Peak\Blueprint\Bedrock\Application;
+use Peak\Common\PhpIni;
+
+class PhpSettings implements Bootable
+{
+    /**
+     * @var Application
+     */
+    private $app;
+
+    /**
+     * PhpSettings constructor.
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Look for php prop to configure php setting on the fly via ini_set
+     */
+    public function boot()
+    {
+        if ($this->app->hasProp('php')) {
+            new PhpIni($this->app->getProp('php'));
+        }
+    }
+}
