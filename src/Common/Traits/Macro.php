8b50ac6c79f7c626609dd2bcf21184f8f49e3682
Franck <snake386@hotmail.com>
2018-12-05 19:20:42 -0500

    remove addMacroFromCallable() and add hasMacro

--- src/Common/Traits/Macro.php
+++ src/Common/Traits/Macro.php
@@ -18,40 +18,36 @@ trait Macro
 
     /**
      * Add a macro
-     * @param string $macroName
+     * @param string $name
      * @param callable $macroCallable
      * @return $this
      */
-    public function addMacro(string $macroName, Closure $macroCallable)
+    public function addMacro(string $name, Closure $macroCallable)
     {
-        $this->macros[$macroName] = Closure::bind($macroCallable, $this, get_class());
+        $this->macros[$name] = Closure::bind($macroCallable, $this, get_class());
         return $this;
     }
 
     /**
-     * Add a macro
-     * @param string $macroName
-     * @param callable $macroCallable
-     * @return $this
+     * @param string $name
+     * @return bool
      */
-    public function addMacroFromCallable(string $macroName, callable $macroCallable)
+    public function hasMacro(string $name)
     {
-        $closure = Closure::fromCallable($macroCallable);
-        $this->macros[$macroName] = Closure::bind($closure, $this, get_class());
-        return $this;
+        return isset($this->macros[$name]);
     }
 
     /**
      * Call a macro
-     * @param string $macroName
+     * @param string $name
      * @param array $args
      * @return mixed
      */
-    public function callMacro(string $macroName, array $args)
+    public function callMacro(string $name, array $args)
     {
-        if (isset($this->macros[$macroName])) {
-            return call_user_func_array($this->macros[$macroName], $args);
+        if (isset($this->macros[$name])) {
+            return call_user_func_array($this->macros[$name], $args);
         }
-        throw new RuntimeException('There is no macro with the given name "'.$macroName.'" to call');
+        throw new RuntimeException('There is no macro with the given name "'.$name.'" to call');
     }
 }
--- tests/Common/Traits/MacroTest.php
+++ tests/Common/Traits/MacroTest.php
@@ -4,13 +4,23 @@ use PHPUnit\Framework\TestCase;
 
 class MacroTest extends TestCase
 {
-    public function testGetArrayFilesContent()
+    public function testAddMacro()
     {
         $a = new MacroTestA();
         $a->addMacro('myFunc', function() {
             return $this->name;
         });
         $this->assertTrue($a->myFunc() === 'bob');
+    }
+
+    public function testHasMacro()
+    {
+        $a = new MacroTestA();
+        $a->addMacro('myFunc', function() {
+            return $this->name;
+        });
+        $this->assertTrue($a->hasMacro('myFunc'));
+        $this->assertFalse($a->hasMacro('myFunc2'));
     }
 
     /**
