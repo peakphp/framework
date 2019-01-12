b293651e077ae71b1a61016ffdc97c229927b036
Franck <snake386@hotmail.com>
2019-01-12 11:19:44 -0500

    updated docblock + added todo

--- src/Config/Processor/XmlProcessor.php
+++ src/Config/Processor/XmlProcessor.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Peak\Config\Processor;
 
 use Peak\Blueprint\Common\ResourceProcessor;
-use Peak\Config\Exception\ProcessorException;
-
 use SimpleXMLElement;
 
 /**
@@ -22,6 +20,7 @@ class XmlProcessor implements ResourceProcessor
     public function process($data): array
     {
         $xml = simplexml_load_string($data, SimpleXMLElement::class, LIBXML_NOCDATA);
+        // @todo handle possibility of json_* returning false
         return json_decode(json_encode($xml),true);
     }
 }
