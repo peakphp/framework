83ab9647dfe000ceff383b9acfdc90576b451384
Franck <snake386@hotmail.com>
2019-01-12 15:41:35 -0500

    updated

--- tests/View/ViewTest.php
+++ tests/View/ViewTest.php
@@ -7,11 +7,33 @@ use Peak\View\Presentation;
 
 class ViewTest extends TestCase
 {
-    public function testCreate()
+    public function testBasic()
     {
         $view = new View(null, $this->createMock(Presentation::class));
         $this->assertTrue(is_array($view->getVars()));
         $this->assertTrue(empty($view->getVars()));
         $this->assertTrue($view->getPresentation() instanceof Presentation);
+    }
+
+    public function testGetVars()
+    {
+        $view = new View(['test' => 'foobar'], $this->createMock(Presentation::class));
+        $vars = $view->getVars();
+        $this->assertTrue(isset($vars['test']));
+        $this->assertTrue($vars['test'] === 'foobar');
+        $this->assertFalse(isset($vars['test2']));
+    }
+
+    public function testVarGetter()
+    {
+        $view = new View(['test' => 'foobar'], $this->createMock(Presentation::class));
+        $this->assertTrue($view->test === 'foobar');
+    }
+
+    public function testVarIsset()
+    {
+        $view = new View(['test' => 'foobar'], $this->createMock(Presentation::class));
+        $this->assertTrue(isset($view->test));
+        $this->assertFalse(isset($view->test2));
     }
 }
