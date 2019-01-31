7c5d7feb8ab05801095ebf774acd0b48acba4825
Franck <snake386@hotmail.com>
2019-01-30 19:40:02 -0500

    refactored ClassInspector

--- src/Di/ClassInspector.php
+++ src/Di/ClassInspector.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Peak\Di;
 
+use Peak\Di\Exception\ClassNotFoundException;
+use Peak\Di\Exception\MethodNotFoundException;
 use \Exception;
 use \ReflectionClass;
-use \ReflectionException;
 
 /**
  * Class ClassInspector
@@ -25,33 +26,36 @@ class ClassInspector
      */
     public function inspect($class, $method = '__construct')
     {
+        if (is_string($class) && !class_exists($class)) {
+            throw new ClassNotFoundException($class);
+        }
         $dependencies = [];
 
-        try {
-            $r = new ReflectionClass($class);
+        $r = new ReflectionClass($class);
 
-            if ($r->hasMethod($method)) {
-                $rp = $r->getMethod($method)->getParameters();
-
-                foreach ($rp as $p) {
-                    $prop = $p->name;
-
-                    $dependencies[$prop] = [];
-                    $dependencies[$prop]['optional'] = $p->isOptional();
-
-                    try {
-                        $class = $p->getClass();
-
-                        if (isset($class)) {
-                            $dependencies[$prop]['class'] = $class->name;
-                        }
-                    } catch (ReflectionException $e) {
-                        $dependencies[$prop]['error'] = $e->getMessage();
-                    }
-                }
+        if (!$r->hasMethod($method)) {
+            if ($method === '__construct') {
+                return $dependencies;
             }
-        } catch (ReflectionException $e) {
-            throw new Exception('Can\'t resolve classname '.$class.'::'.$method);
+            throw new MethodNotFoundException($class, $method);
+        }
+
+        $rp = $r->getMethod($method)->getParameters();
+
+        foreach ($rp as $p) {
+            $prop = $p->name;
+
+            $dependencies[$prop] = [];
+            $dependencies[$prop]['optional'] = $p->isOptional();
+
+            try {
+                $class = $p->getClass();
+                if (isset($class)) {
+                    $dependencies[$prop]['class'] = $class->name;
+                }
+            } catch (\Exception $e) {
+                $dependencies[$prop]['error'] = $e->getMessage();
+            }
         }
 
         return $dependencies;
