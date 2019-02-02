00753ce7fbbf48a5afd3ed7b53503fc8322a95c3
Franck <snake386@hotmail.com>
2019-02-02 09:35:29 -0500

    updated

--- build/split-notags.sh
+++ build/split-notags.sh
@@ -4,7 +4,6 @@ git subsplit publish --no-tags --heads="master" src/Blueprint:git@github.com:pea
 git subsplit publish --no-tags --heads="master" src/Collection:git@github.com:peakphp/collection.git
 git subsplit publish --no-tags --heads="master" src/Common:git@github.com:peakphp/common.git
 git subsplit publish --no-tags --heads="master" src/Config:git@github.com:peakphp/config.git
-git subsplit publish --no-tags --heads="master" src/DebugBar:git@github.com:peakphp/debugbar.git
 git subsplit publish --no-tags --heads="master" src/Di:git@github.com:peakphp/di.git
 git subsplit publish --no-tags --heads="master" src/Http:git@github.com:peakphp/http.git
 git subsplit publish --no-tags --heads="master" src/Pipeline:git@github.com:peakphp/pipeline.git
