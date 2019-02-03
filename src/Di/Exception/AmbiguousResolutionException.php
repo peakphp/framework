6233985032dc9e761017615fd30ec595c0a378c6
Franck <snake386@hotmail.com>
2019-02-03 09:43:26 -0500

    implements ContainerExceptionInterface

--- src/Di/Exception/AmbiguousResolutionException.php
+++ src/Di/Exception/AmbiguousResolutionException.php
@@ -4,18 +4,19 @@ declare(strict_types=1);
 
 namespace Peak\Di\Exception;
 
+use Psr\Container\ContainerExceptionInterface;
 use \Exception;
 
-class AmbiguousResolutionException extends Exception
+class AmbiguousResolutionException extends Exception implements ContainerExceptionInterface
 {
     /**
      * AmbiguousResolutionException constructor.
      * @param string $interface
      * @param array $instance
      */
-    public function __construct(string $interface, array $instance)
+    public function __construct(string $interface, array $instances)
     {
-        parent::__construct('Dependencies for interface '.$interface.' is ambiguous. There is '.count($instance).' different stored instances for this interface.');
+        parent::__construct('Dependencies for interface ['.$interface.'] is ambiguous. There is '.count($instances).' different stored instances for this interface.');
     }
 }
 
