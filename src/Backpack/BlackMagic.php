e0844b4f7bed3e4529de371e335e73bc99c955fd
Franck <snake386@hotmail.com>
2019-02-04 17:38:28 -0500

    removed deprecated Backpack\Application

--- src/Backpack/BlackMagic.php
+++ src/Backpack/BlackMagic.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Peak\Backpack;
 
+use Peak\Bedrock\Application\Application;
 use Peak\Http\Request\HandlerResolver;
 use Peak\Http\Stack;
 use Peak\Http\Response\Emitter;
