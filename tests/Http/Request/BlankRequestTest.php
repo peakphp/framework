54e8869141ca153cef6f2b6bcd865e09b6a8037d
Franck <snake386@hotmail.com>
2019-02-05 15:31:52 -0500

    updated

--- tests/Http/Request/BlankRequestTest.php
+++ tests/Http/Request/BlankRequestTest.php
@@ -9,28 +9,28 @@ class BlankRequestTest extends TestCase
     public function testNull()
     {
         $blankRequest = new BlankRequest();
-        $this->assertNull($blankRequest->getAttribute('blabla'));
-        $this->assertNull($blankRequest->getAttributes());
-        $this->assertNull($blankRequest->getBody());
-        $this->assertNull($blankRequest->getCookieParams());
-        $this->assertNull($blankRequest->getHeader('test'));
-        $this->assertNull($blankRequest->getHeaderLine('test'));
-        $this->assertNull($blankRequest->getHeaders());
-        $this->assertNull($blankRequest->getMethod());
-        $this->assertNull($blankRequest->getParsedBody());
-        $this->assertNull($blankRequest->getProtocolVersion());
-        $this->assertNull($blankRequest->getQueryParams());
-        $this->assertNull($blankRequest->getRequestTarget());
-        $this->assertNull($blankRequest->getServerParams());
-        $this->assertNull($blankRequest->getUploadedFiles());
-        $this->assertNull($blankRequest->getUri());
-        $this->assertNull($blankRequest->withRequestTarget('test'));
-        $this->assertNull($blankRequest->withHeader('test', 'test'));
-        $this->assertNull($blankRequest->withAddedHeader('test', 'test'));
-        $this->assertNull($blankRequest->withoutHeader('test'));
-        $this->assertNull($blankRequest->withAttribute('test', 'test'));
-        $this->assertNull($blankRequest->withoutAttribute('test'));
-        $this->assertNull($blankRequest->withParsedBody('test'));
+        $this->assertTrue(is_null($blankRequest->getAttribute('blabla')));
+        $this->assertTrue(is_array($blankRequest->getAttributes()));
+        $this->assertTrue(is_null($blankRequest->getBody()));
+        $this->assertTrue(is_array($blankRequest->getCookieParams()));
+        $this->assertTrue(is_array($blankRequest->getHeader('test')));
+        $this->assertTrue(is_string($blankRequest->getHeaderLine('test')));
+        $this->assertTrue(is_array($blankRequest->getHeaders()));
+        $this->assertTrue(is_string($blankRequest->getMethod()));
+        $this->assertTrue(is_null($blankRequest->getParsedBody()));
+        $this->assertTrue(is_string($blankRequest->getProtocolVersion()));
+        $this->assertTrue(is_array($blankRequest->getQueryParams()));
+        $this->assertTrue(is_string($blankRequest->getRequestTarget()));
+        $this->assertTrue(is_array($blankRequest->getServerParams()));
+        $this->assertTrue(is_array($blankRequest->getUploadedFiles()));
+        $this->assertTrue(is_null($blankRequest->getUri()));
+        $this->assertTrue($blankRequest->withRequestTarget('test') instanceof BlankRequest);
+        $this->assertTrue($blankRequest->withHeader('test', 'test') instanceof BlankRequest);
+        $this->assertTrue($blankRequest->withAddedHeader('test', 'test') instanceof BlankRequest);
+        $this->assertTrue($blankRequest->withoutHeader('test') instanceof BlankRequest);
+        $this->assertTrue($blankRequest->withAttribute('test', 'test') instanceof BlankRequest);
+        $this->assertTrue($blankRequest->withoutAttribute('test') instanceof BlankRequest);
+        $this->assertTrue($blankRequest->withParsedBody('test') instanceof BlankRequest);
     }
 
 }
