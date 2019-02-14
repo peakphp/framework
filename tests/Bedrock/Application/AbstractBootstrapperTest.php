947c37143e1e30dbedf4faf1f7263fcc2b4a3906
Franck <snake386@hotmail.com>
2019-02-13 21:08:22 -0500

    updated

--- tests/Bedrock/Application/AbstractBootstrapperTest.php
+++ tests/Bedrock/Application/AbstractBootstrapperTest.php
@@ -3,7 +3,8 @@
 use \PHPUnit\Framework\TestCase;
 use \Peak\Bedrock\Kernel;
 use \Peak\Bedrock\Application\Application;
-use \Peak\Bedrock\Application\AbstractBootstrapper;
+use \Peak\Di\Container;
+use \Peak\Http\Request\HandlerResolver;
 
 require_once FIXTURES_PATH . '/application/Bootstrapper.php';
 
@@ -12,19 +13,14 @@ require_once FIXTURES_PATH . '/application/Bootstrapper.php';
  */
 class AbstractBootstrapperTest extends TestCase
 {
+    /**
+     * @throws Exception
+     */
     public function testCreate()
     {
-        $app = $this->createMock(Application::class);
-
-        $kernel = $this->createMock(Kernel::class);
-        $kernel->expects($this->once())
-            ->method('getEnv')
-            ->will($this->returnValue('dev'));
-
-        $app->expects($this->once())
-            ->method('getKernel')
-            ->will($this->returnValue($kernel));
-
+        $kernel = new Kernel('dev', new Container);
+        $handlerResolver = $this->createMock(HandlerResolver::class);
+        $app = new Application($kernel, $handlerResolver);
         $bootstrapper = new Bootstrapper($app);
 
         $this->assertTrue(empty($_GET));
