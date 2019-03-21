e20f8d96206b92ae8bc83632bb3bc2555e4c7aa1
Franck <snake386@hotmail.com>
2019-03-20 23:21:15 -0400

    updated

--- tests/Http/Request/RouteServerRequestTest.php
+++ tests/Http/Request/RouteServerRequestTest.php
@@ -19,4 +19,59 @@ class RouteServerRequestTest extends TestCase
         $this->assertTrue($rsr->getParam('test') == 123);
         $this->assertTrue($rsr->getParam('foobar', 'test') === 'test');
     }
+
+    public function testHasWithNoRouteParam()
+    {
+        $request = $this->createMock(ServerRequestInterface::class);
+        $rsr = new RouteServerRequest($request);
+
+        $this->assertFalse($rsr->hasParam('foobar'));
+    }
+
+    public function testHasStrict()
+    {
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request->param = new RouteParameter(['test' => 123]);
+        $rsr = new RouteServerRequest($request);
+        $this->assertFalse($rsr->hasParam('test', 'string'));
+    }
+
+    public function testInterfaceSignature()
+    {
+        $request = $this->createMock(ServerRequestInterface::class);
+        $rsr = new RouteServerRequest($request);
+
+        $rsr->getProtocolVersion();
+        //$rsr->withProtocolVersion("");
+        $rsr->getHeaders();
+        $rsr->hasHeader("");
+        $rsr->getHeader("");
+        $rsr->getHeaderLine("");
+        //$rsr->withHeader("", "");
+        //$rsr->withAddedHeader("", "");
+        //$rsr->withoutHeader("");
+        $rsr->getBody();
+        //$rsr->withBody($this->createMock(\Psr\Http\Message\StreamInterface::class));
+        $rsr->getRequestTarget();
+        //$rsr->withRequestTarget("");
+        $rsr->getMethod();
+        //$rsr->withMethod("");
+        $rsr->getUri();
+        //$rsr->withUri($this->createMock(\Psr\Http\Message\UriInterface::class));
+        $rsr->getServerParams();
+        $rsr->getCookieParams();
+        //$rsr->withCookieParams([]);
+        $rsr->getQueryParams();
+        //$rsr->withQueryParams([]);
+        $rsr->getUploadedFiles();
+        //$rsr->withUploadedFiles([]);
+        $rsr->getParsedBody();
+        //$rsr->withParsedBody("");
+        $rsr->getAttributes();
+        $rsr->getAttribute("", null);
+        //$rsr->withAttribute("", "");
+        //$rsr->withoutAttribute("");
+        $this->assertInstanceOf(ServerRequestInterface::class, $rsr);
+
+    }
 }
