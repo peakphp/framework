7049ee241b57e625a7e4bba1723b1c5bdaef9a6b
Franck <snake386@hotmail.com>
2019-06-19 20:35:22 -0400

    moved component pipeline outside the framework

--- build/split-notags.sh
+++ build/split-notags.sh
@@ -6,5 +6,4 @@ git subsplit publish --no-tags --heads="master" src/Common:git@github.com:peakph
 git subsplit publish --no-tags --heads="master" src/Config:git@github.com:peakphp/config.git
 git subsplit publish --no-tags --heads="master" src/Di:git@github.com:peakphp/di.git
 git subsplit publish --no-tags --heads="master" src/Http:git@github.com:peakphp/http.git
-git subsplit publish --no-tags --heads="master" src/Pipeline:git@github.com:peakphp/pipeline.git
 rm -rf .subsplit/
