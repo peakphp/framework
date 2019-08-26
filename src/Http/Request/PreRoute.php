fe77f66225a04dd1e345dbee42405062654c094c
Franck <snake386@hotmail.com>
2019-08-26 08:49:42 -0400

    added constructor

--- src/Http/Request/PreRoute.php
+++ src/Http/Request/PreRoute.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace Peak\Http\Request;
 
+use Peak\Blueprint\Http\Stack;
 use Psr\Http\Message\ServerRequestInterface;
 
 class PreRoute extends Route
 {
+    /**
+     * PreRoute constructor.
+     * @param string $path
+     * @param Stack $stack
+     */
+    public function __construct(string $path, Stack $stack)
+    {
+        parent::__construct(null, $path, $stack);
+    }
+
     /**
      * @param ServerRequestInterface $request
      * @return bool
