60e894e84479a7bb5db9ace0661f370e5eee5aef
Franck <snake386@hotmail.com>
2019-09-14 08:32:10 -0400

    updated

--- tests/Bedrock/KernelTest.php
+++ tests/Bedrock/KernelTest.php
@@ -12,7 +12,7 @@ class KernelTest extends TestCase
 {
     public function testVersion()
     {
-        $this->assertTrue(Kernel::VERSION === '4.0.0');
+        $this->assertTrue(Kernel::VERSION === '4.1.0');
     }
 
     public function testInstantiation()
