489443579c0f8378b8cd99582fada5968b5b0c4b
Franck <snake386@hotmail.com>
2020-02-21 22:45:05 -0500

    update version

--- src/Bedrock/Kernel.php
+++ src/Bedrock/Kernel.php
@@ -12,7 +12,7 @@ class Kernel implements \Peak\Blueprint\Bedrock\Kernel
      * Peak kernel version
      * @var string
      */
-    const VERSION = '4.2.2';
+    const VERSION = '4.2.3';
 
     /**
      * @var ContainerInterface
